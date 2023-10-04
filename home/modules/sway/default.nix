a02be63bc8c26775504504ecb0c7ce8fba24458d
bashfulrobot <dustin@bashfulrobot.com>
2023-10-04 16:09:29 -0700

    feat: Refactor and customize Swappy configuration
    - Add config for swappy, a screenshot utility
    - create a default config file
    
    Changes to be committed:
    	modified:   home/modules/sway/default.nix

--- home/modules/sway/default.nix
+++ home/modules/sway/default.nix
@@ -5,6 +5,19 @@
     # ./sway-alttab
   ];
 
+  home.file.".config/swappy/config".text = ''
+    [Default]
+    save_dir=$HOME/Pictures/Screenshots
+    save_filename_format=ss-%Y%m%d-%H%M%S.png
+    show_panel=true
+    line_size=5
+    text_size=20
+    text_font=sans-serif
+    paint_mode=arrow
+    early_exit=true
+    fill_shape=false
+  '';
+
   home.file.".config/sway/config".text = ''
         # Default config for sway
     #
