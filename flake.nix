688e1531b1ba113f5cc1182f878ea4b3808cdc30
bashfulrobot <dustin@bashfulrobot.com>
2023-10-10 08:56:24 -0700

    refactor: Refactor system configuration for `nixdo`
    - Remove unnecessary code for the `nixdo` system configuration in flake.nix file.
    
    Changes to be committed:
    	modified:   flake.nix

--- flake.nix
+++ flake.nix
@@ -81,21 +81,7 @@
         nixdo = nixpkgs.lib.nixosSystem {
           specialArgs = { inherit inputs; };
           system = "x86_64-linux";
-          modules = [
-            ./hosts/nixdo
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.users.dustin = { imports = [ ./home/nixdo ]; };
-
-              # Overlays
-              nixpkgs.overlays = [ nur.overlay ];
-
-              # Allow unfree packages
-              nixpkgs.config.allowUnfree = true;
-            }
-          ];
+          modules = [ ./hosts/nixdo ];
         };
 
       };
