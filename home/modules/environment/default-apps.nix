e58d587c92c42b9e3c6c691e59344daa8c4e0573
bashfulrobot <dustin@bashfulrobot.com>
2023-10-10 17:06:04 -0700

    refactor: Update default applications to Chromium Browser.
    - Update default applications for various file types to use Chromium Browser instead of Microsoft Edge
    - Make necessary changes in [home/modules/environment/default-apps.nix] file
    - Improve user experience by providing a more widely-used browser as the default option
    
    [home/modules/environment/default-apps.nix]
    - Change the default applications for various file types from Microsoft Edge to Chromium Browser
    Changes to be committed:
    	modified:   home/modules/environment/default-apps.nix

--- home/modules/environment/default-apps.nix
+++ home/modules/environment/default-apps.nix
@@ -1,40 +1,40 @@
 { config, ... }: {
   home.file.".config/mimeapps.list".text = ''
     [Default Applications]
-    x-scheme-handler/http=microsoft-edge.desktop
-    x-scheme-handler/https=microsoft-edge.desktop
-    text/html=microsoft-edge.desktop
-    application/xhtml+xml=microsoft-edge.desktop
+    x-scheme-handler/http=chromium-browser.desktop
+    x-scheme-handler/https=chromium-browser.desktop
+    text/html=chromium-browser.desktop
+    application/xhtml+xml=chromium-browser.desktop
     x-scheme-handler/mailspring=Mailspring.desktop
-    x-scheme-handler/about=microsoft-edge.desktop
-    x-scheme-handler/unknown=microsoft-edge.desktop
-    x-scheme-handler/webcal=microsoft-edge.desktop
+    x-scheme-handler/about=chromium-browser.desktop
+    x-scheme-handler/unknown=chromium-browser.desktop
+    x-scheme-handler/webcal=chromium-browser.desktop
     text/markdown=code.desktop
     x-scheme-handler/tootle=com.github.bleakgrey.tootle.desktop
     application/x-yaml=code.desktop
-    x-scheme-handler/mailto=microsoft-edge.desktop
+    x-scheme-handler/mailto=chromium-browser.desktop
     text/plain=code.desktop
     text/x-yaml=code.desktop
     application/x-shellscript=code.desktop
-    x-scheme-handler/chrome=microsoft-edge.desktop
-    application/x-extension-htm=microsoft-edge.desktop
-    application/x-extension-html=microsoft-edge.desktop
-    application/x-extension-shtml=microsoft-edge.desktop
-    application/x-extension-xhtml=microsoft-edge.desktop
-    application/x-extension-xht=microsoft-edge.desktop
+    x-scheme-handler/chrome=chromium-browser.desktop
+    application/x-extension-htm=chromium-browser.desktop
+    application/x-extension-html=chromium-browser.desktop
+    application/x-extension-shtml=chromium-browser.desktop
+    application/x-extension-xhtml=chromium-browser.desktop
+    application/x-extension-xht=chromium-browser.desktop
     application/pdf=org.gnome.Evince.desktop
 
     [Added Associations]
-    application/xhtml+xml=microsoft-edge.desktop;microsoft-edge.desktop;
+    application/xhtml+xml=chromium-browser.desktop;chromium-browser.desktop;
     text/markdown=code.desktop;
     application/x-yaml=code.desktop;
     text/plain=code.desktop;
     application/x-shellscript=code.desktop;
-    x-scheme-handler/chrome=microsoft-edge.desktop;
-    application/x-extension-htm=microsoft-edge.desktop;
-    application/x-extension-html=microsoft-edge.desktop;
-    application/x-extension-shtml=microsoft-edge.desktop;
-    application/x-extension-xhtml=microsoft-edge.desktop;
-    application/x-extension-xht=microsoft-edge.desktop;
+    x-scheme-handler/chrome=chromium-browser.desktop;
+    application/x-extension-htm=chromium-browser.desktop;
+    application/x-extension-html=chromium-browser.desktop;
+    application/x-extension-shtml=chromium-browser.desktop;
+    application/x-extension-xhtml=chromium-browser.desktop;
+    application/x-extension-xht=chromium-browser.desktop;
   '';
 }
