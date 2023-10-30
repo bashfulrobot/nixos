43b3034a6ce24a29322d4bf646d75384427893f0
bashfulrobot <dustin@bashfulrobot.com>
2023-10-29 22:41:41 -0700

    chore: Update GNOME desktop and shell settings
    - Added new settings to the org/gnome/desktop/interface and org/gnome/shell/extensions/unite settings
    - Updated the picture-uri in the org/gnome/desktop/background and org/gnome/desktop/screensaver settings
    - Added new settings to the org/gnome/mutter and org/gnome/desktop/peripherals/touchpad settings
    - Changed the natural-scroll setting in the org/gnome/desktop/peripherals/mouse settings
    - Added new settings to the org/gnome/settings-daemon/plugins/color settings
    - Updated the enabled-extensions setting in the org/gnome/shell settings
    - Updated the name setting in the org/gnome/shell/extensions/user-theme settings
    - Added new settings to the org/gnome/shell/extensions/quick-settings-audio-panel and org/gnome/shell/extensions/just-perfection settings
    - Updated the keybindings in the org/gnome/desktop/wm/keybindings settings
    - Added a custom keybinding in the org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0 settings
    
    [home/modules/gnome/default.nix]
    - Changed the `color-scheme` in the `org/gnome/desktop/interface` settings to "prefer-dark"
    - Added `cursor-theme`, `enable-animations`, `gtk-theme`, `icon-theme`, and `locate-pointer` settings to the `org/gnome/desktop/interface` settings
    - Added `enable-titlebar-actions`, `greyscale-tray-icons`, `hide-activities-button`, `hide-app-menu-icon`, `hide-dropdown-arrows`, `hide-window-titlebars`, `notifications-position`, `reduce-panel-spacing`, `show-desktop-name`, `show-legacy-tray`, `show-window-buttons`, `show-window-title`, and `window-buttons-theme` settings to the `org/gnome/shell/extensions/unite` settings
    - Added `bluetooth-auto-power-on` setting to the `org/gnome/shell/extensions/bluetooth-quick-connect` settings
    - Updated the `picture-uri` in the `org/gnome/desktop/background` settings
    - Updated the `picture-uri` in the `org/gnome/desktop/screensaver` settings
    - Added `center-new-windows` setting to the `org/gnome/mutter` settings
    - Added `two-finger-scrolling-enabled` setting to the `org/gnome/desktop/peripherals/touchpad` settings
    - Changed the `natural-scroll` setting in the `org/gnome/desktop/peripherals/mouse` settings to `false`
    - Added `night-light-enabled` setting to the `org/gnome/settings-daemon/plugins/color` settings
    - Updated the `enabled-extensions` setting in the `org/gnome/shell` settings
    - Updated the `name` setting in the `org/gnome/shell/extensions/user-theme` settings
    - Added `media-control`, `merge-panel`, `move-master-volume`, and `panel-position` settings to the `org/gnome/shell/extensions/quick-settings-audio-panel` settings
    - Added various settings to the `org/gnome/shell/extensions/just-perfection` settings
    - Updated the keybindings in the `org/gnome/desktop/wm/keybindings` settings
    - Added a custom keybinding in the `org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0` settings

--- home/modules/gnome/default.nix
+++ home/modules/gnome/default.nix
@@ -7,12 +7,33 @@ with lib.hm.gvariant;
 
     "org/gnome/desktop/interface" = {
       color-scheme = "prefer-dark";
-      font-hinting = "full";
+      cursor-theme = "Yaru";
+      enable-animations = true;
       font-antialiasing = "rgba";
+      font-hinting = "full";
       gtk-theme = "Yaru-blue-dark";
       icon-theme = "Yaru-bark-dark";
-      cursor-theme = "Yaru";
       locate-pointer = true;
+    };
+
+    "org/gnome/shell/extensions/unite" = {
+      enable-titlebar-actions = true;
+      greyscale-tray-icons = true;
+      hide-activities-button = "always";
+      hide-app-menu-icon = true;
+      hide-dropdown-arrows = true;
+      hide-window-titlebars = "maximized";
+      notifications-position = "right";
+      reduce-panel-spacing = true;
+      show-desktop-name = false;
+      show-legacy-tray = true;
+      show-window-buttons = "maximized";
+      show-window-title = "never";
+      window-buttons-theme = "auto";
+    };
+
+    "org/gnome/shell/extensions/bluetooth-quick-connect" = {
+      bluetooth-auto-power-on = true;
     };
 
     "org/gnome/desktop/background" = {
@@ -37,10 +58,36 @@ with lib.hm.gvariant;
 
     "org/gnome/shell" = {
       enabled-extensions =
-        "['user-theme@gnome-shell-extensions.gcampax.github.com']";
+        "['quick-settings-audio-panel@rayzeq.github.io', 'unite@hardpixel.eu', 'appindicatorsupport@rgcjonas.gmail.com', 'bluetooth-quick-connect@bjarosze.gmail.com', 'blur-my-shell@aunetx', 'just-perfection-desktop@just-perfection', 'user-theme@gnome-shell-extensions.gcampax.github.com']";
     };
 
     "org/gnome/shell/extensions/user-theme" = { name = "Yaru-blue-dark"; };
+
+    "org/gnome/shell/extensions/quick-settings-audio-panel" = {
+      media-control = "move";
+      merge-panel = true;
+      move-master-volume = true;
+      panel-position = "bottom";
+
+    };
+
+    "org/gnome/shell/extensions/just-perfection" = {
+      theme = true;
+      window-picker-icon = true;
+      window-demands-attention-focus = false;
+      startup-status = 1;
+      ripple-box = true;
+      dash-icon-size = 0;
+      app-menu-icon = true;
+      workspaces-in-app-grid = true;
+      workspace = true;
+      workspace-background-corner-size = 0;
+      show-apps-button = true;
+      search = true;
+      app-menu = true;
+      panel = true;
+      double-super-to-appgrid = true;
+    };
 
     "org/gnome/desktop/wm/keybindings" = {
       close = "['<Super>q']";
