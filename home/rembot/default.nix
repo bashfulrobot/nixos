a4e49bc089f787d5c25241c017b3967d909ee31b
bashfulrobot <dustin@bashfulrobot.com>
2023-12-03 15:40:03 -0800

    chore: Update NixOS state version to "23.11"
    - Update NixOS state version from "23.05" to "23.11"
    - Change value of `home.stateVersion` from "23.05" to "23.11" in multiple files
    - Update `system.stateVersion` from "23.05" to "23.11"
    
    [hosts/modules/default.nix]
    - Update the NixOS state version from "23.05" to "23.11"
    [home/dustin-krysak/default.nix]
    - Changed the value of `home.stateVersion` from "23.05" to "23.11"
    [hosts/nixdo/default.nix]
    - Updated the `system.stateVersion` from "23.05" to "23.11"
    [home/rembot/default.nix]
    - Change the value of `home.stateVersion` from "23.05" to "23.11"

--- home/rembot/default.nix
+++ home/rembot/default.nix
@@ -16,6 +16,6 @@
   # You can update Home Manager without changing this value. See
   # the Home Manager release notes for a list of state version
   # changes in each release.
-  home.stateVersion = "23.05";
+  home.stateVersion = "23.11";
   programs.home-manager.enable = true;
 }
