9f3111adbaa30cfd5ceb23eb2378a706e4680cc3
bashfulrobot <dustin@bashfulrobot.com>
2023-12-05 23:48:33 -0800

    feat: Update default applications from Firefox to Chromium
    - Update default applications from Firefox to Chromium Browser for various schemes and file types in `default-apps.nix` file in `home/modules/environment`
    - Addressed large changes in some files not captured in diff summary
    - Overall improvements and updates made in this commit
    
    [home/modules/environment/default-apps.nix]
    - Changed default applications from Firefox to Chromium Browser for various schemes and file types

--- home/modules/environment/default-apps.nix
+++ home/modules/environment/default-apps.nix
@@ -1,40 +1,40 @@
 { config, ... }: {
   home.file.".config/mimeapps.list".text = ''
     [Default Applications]
-    x-scheme-handler/http=firefox.desktop
-    x-scheme-handler/https=firefox.desktop
-    text/html=firefox.desktop
-    application/xhtml+xml=firefox.desktop
+    x-scheme-handler/http=chromium-browser.desktop
+    x-scheme-handler/https=chromium-browser.desktop
+    text/html=chromium-browser.desktop
+    application/xhtml+xml=chromium-browser.desktop
     x-scheme-handler/mailspring=Mailspring.desktop
-    x-scheme-handler/about=firefox.desktop
-    x-scheme-handler/unknown=firefox.desktop
-    x-scheme-handler/webcal=firefox.desktop
+    x-scheme-handler/about=chromium-browser.desktop
+    x-scheme-handler/unknown=chromium-browser.desktop
+    x-scheme-handler/webcal=chromium-browser.desktop
     text/markdown=code.desktop
     x-scheme-handler/tootle=com.github.bleakgrey.tootle.desktop
     application/x-yaml=code.desktop
-    x-scheme-handler/mailto=firefox.desktop
+    x-scheme-handler/mailto=chromium-browser.desktop
     text/plain=code.desktop
     text/x-yaml=code.desktop
     application/x-shellscript=code.desktop
-    x-scheme-handler/chrome=firefox.desktop
-    application/x-extension-htm=firefox.desktop
-    application/x-extension-html=firefox.desktop
-    application/x-extension-shtml=firefox.desktop
-    application/x-extension-xhtml=firefox.desktop
-    application/x-extension-xht=firefox.desktop
+    x-scheme-handler/chrome=chromium-browser.desktop
+    application/x-extension-htm=chromium-browser.desktop
+    application/x-extension-html=chromium-browser.desktop
+    application/x-extension-shtml=chromium-browser.desktop
+    application/x-extension-xhtml=chromium-browser.desktop
+    application/x-extension-xht=chromium-browser.desktop
     application/pdf=org.gnome.Evince.desktop
 
     [Added Associations]
-    application/xhtml+xml=firefox.desktop;firefox.desktop;
+    application/xhtml+xml=chromium-browser.desktop;chromium-browser.desktop;
     text/markdown=code.desktop;
     application/x-yaml=code.desktop;
     text/plain=code.desktop;
     application/x-shellscript=code.desktop;
-    x-scheme-handler/chrome=firefox.desktop;
-    application/x-extension-htm=firefox.desktop;
-    application/x-extension-html=firefox.desktop;
-    application/x-extension-shtml=firefox.desktop;
-    application/x-extension-xhtml=firefox.desktop;
-    application/x-extension-xht=firefox.desktop;
+    x-scheme-handler/chrome=chromium-browser.desktop;
+    application/x-extension-htm=chromium-browser.desktop;
+    application/x-extension-html=chromium-browser.desktop;
+    application/x-extension-shtml=chromium-browser.desktop;
+    application/x-extension-xhtml=chromium-browser.desktop;
+    application/x-extension-xht=chromium-browser.desktop;
   '';
 }
