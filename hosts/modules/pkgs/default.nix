ec6c577071e09d28c6c909aedb3471d2bd758adc
bashfulrobot <dustin@bashfulrobot.com>
2023-12-19 13:14:32 -0800

    feat: Update system package list
    - Added `jetbrains.goland` and `teamviewer` to the system packages
    
    [hosts/modules/pkgs/default.nix]
    - Added `jetbrains.goland` to the list of system packages
    Changes to be committed:
    modified:   hosts/modules/pkgs/default.nix

--- hosts/modules/pkgs/default.nix
+++ hosts/modules/pkgs/default.nix
@@ -80,6 +80,7 @@
     nodejs-18_x # JavaScript runtime
     # postman # API testing tool
     vscode # code editor
+    jetbrains.goland # Go IDE
     #sublime4 # code editor
     #sublime-merge # GUI Git Client
     # Causes:error: Package ‘python-2.7.18.6’ in /nix/store/zb2695k9v4hmjdki97p0yhd4mys270fs-source/pkgs/development/interpreters/python/cpython/2.7/default.nix:330 is marked as insecure, refusing to evaluate.
