00220f57b13039a7ed4d6e6db11463b0899ca1f4
bashfulrobot <dustin@bashfulrobot.com>
2023-12-20 23:17:53 -0800

    refactor: Improve boot menu display and sysctl configuration
    - Improve boot menu display by enabling `systemd-boot` and setting `consoleMode` to "max"
    - Disable `netbootxyz` in `systemd-boot` for better performance
    - Allow touching of EFI variables with `efi.canTouchEfiVariables`
    - Enable forwarding of traffic between interfaces in the kernel sysctl configuration
    
    [hosts/rembot/boot.nix]
    - Enable `systemd-boot` and set `consoleMode` to "max" for improved boot menu display
    - Disable `netbootxyz` in `systemd-boot`
    - Allow touching of EFI variables in `efi.canTouchEfiVariables`
    - Enable forwarding of traffic between interfaces in the kernel sysctl configuration

--- hosts/rembot/boot.nix
+++ hosts/rembot/boot.nix
@@ -1,13 +1,22 @@
 { config, pkgs, ... }: {
 
   # Bootloader.
-  boot.loader.systemd-boot = {
-    enable = true;
-    consoleMode = "max"; # ensure windows will be found in boot menu
-    netbootxyz.enable = false;
+  boot = {
+    loader = {
+      systemd-boot = {
+        enable = true;
+        consoleMode = "max"; # ensure windows will be found in boot menu
+        netbootxyz.enable = false;
+      };
+      efi.canTouchEfiVariables = true;
+    };
+    kernel = {
+      sysctl = {
+        # Enable traffic between interfaces
+        "net.ipv4.conf.all.forwarding" = true;
+        "net.ipv6.conf.all.forwarding" = true;
+      };
+    };
   };
-
-
-  boot.loader.efi.canTouchEfiVariables = true;
 
 }
