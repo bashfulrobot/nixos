297734f4e0da6e5ce8badf596d40a25a90e3596d
bashfulrobot <dustin@bashfulrobot.com>
2023-12-27 13:30:12 -0800

    build: Update `stripRoot` setting in `default.nix` file
    - Update `default.nix` file in `home/modules/dagger/build` directory
    - Change `stripRoot` from `true` to `false` in the `fetchzip` block.
    
    [home/modules/dagger/build/default.nix]
    - Change `stripRoot` from `true` to `false` in the `fetchzip` block

--- home/modules/dagger/build/default.nix
+++ home/modules/dagger/build/default.nix
@@ -8,12 +8,12 @@ in pkgs.stdenv.mkDerivation {
     url =
       "https://github.com/dagger/dagger/releases/download/v${version}/dagger_v${version}_linux_amd64.tar.gz";
     sha256 = "sha256-RipH7gy9IL/xRlZy+KHIz8WfK1vrRUOdmY0G/rbc19c=";
-    stripRoot = true;
+    stripRoot = false;
   };
   phases = [ "installPhase" ];
   installPhase = ''
     mkdir -p $out/bin
-    cp -r $src/dagger $out/bin/dagger
+    cp $src/dagger $out/bin/dagger
     chmod +x $out/bin/dagger
   '';
 
