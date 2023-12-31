8a8f0752669cff2e1a6313cc1916af91a19cccc9
bashfulrobot <dustin@bashfulrobot.com>
2023-12-31 13:26:59 -0800

    feat: Update default browser to Vivaldi
    - Change the default browser from `chromium-browser` to `vivaldi` in `default-apps.nix`
    - Update the necessary dependencies and configurations for the change
    - Ensure compatibility with existing modules and applications
    
    [home/modules/environment/default-apps.nix]
    - Change the default browser from `chromium-browser` to `vivaldi`

--- home/modules/environment/default-apps.nix
+++ home/modules/environment/default-apps.nix
@@ -1,40 +1,40 @@
 { config, ... }: {
   home.file.".config/mimeapps.list".text = ''
     [Default Applications]
-    x-scheme-handler/http=chromium-browser.desktop
-    x-scheme-handler/https=chromium-browser.desktop
-    text/html=chromium-browser.desktop
-    application/xhtml+xml=chromium-browser.desktop
+    x-scheme-handler/http=vivaldi.desktop
+    x-scheme-handler/https=vivaldi.desktop
+    text/html=vivaldi.desktop
+    application/xhtml+xml=vivaldi.desktop
     x-scheme-handler/mailspring=Mailspring.desktop
-    x-scheme-handler/about=chromium-browser.desktop
-    x-scheme-handler/unknown=chromium-browser.desktop
-    x-scheme-handler/webcal=chromium-browser.desktop
+    x-scheme-handler/about=vivaldi.desktop
+    x-scheme-handler/unknown=vivaldi.desktop
+    x-scheme-handler/webcal=vivaldi.desktop
     text/markdown=code.desktop
     x-scheme-handler/tootle=com.github.bleakgrey.tootle.desktop
     application/x-yaml=code.desktop
-    x-scheme-handler/mailto=chromium-browser.desktop
+    x-scheme-handler/mailto=vivaldi.desktop
     text/plain=code.desktop
     text/x-yaml=code.desktop
     application/x-shellscript=code.desktop
-    x-scheme-handler/chrome=chromium-browser.desktop
-    application/x-extension-htm=chromium-browser.desktop
-    application/x-extension-html=chromium-browser.desktop
-    application/x-extension-shtml=chromium-browser.desktop
-    application/x-extension-xhtml=chromium-browser.desktop
-    application/x-extension-xht=chromium-browser.desktop
+    x-scheme-handler/chrome=vivaldi.desktop
+    application/x-extension-htm=vivaldi.desktop
+    application/x-extension-html=vivaldi.desktop
+    application/x-extension-shtml=vivaldi.desktop
+    application/x-extension-xhtml=vivaldi.desktop
+    application/x-extension-xht=vivaldi.desktop
     application/pdf=org.gnome.Evince.desktop
 
     [Added Associations]
-    application/xhtml+xml=chromium-browser.desktop;chromium-browser.desktop;
+    application/xhtml+xml=vivaldi.desktop;vivaldi.desktop;
     text/markdown=code.desktop;
     application/x-yaml=code.desktop;
     text/plain=code.desktop;
     application/x-shellscript=code.desktop;
-    x-scheme-handler/chrome=chromium-browser.desktop;
-    application/x-extension-htm=chromium-browser.desktop;
-    application/x-extension-html=chromium-browser.desktop;
-    application/x-extension-shtml=chromium-browser.desktop;
-    application/x-extension-xhtml=chromium-browser.desktop;
-    application/x-extension-xht=chromium-browser.desktop;
+    x-scheme-handler/chrome=vivaldi.desktop;
+    application/x-extension-htm=vivaldi.desktop;
+    application/x-extension-html=vivaldi.desktop;
+    application/x-extension-shtml=vivaldi.desktop;
+    application/x-extension-xhtml=vivaldi.desktop;
+    application/x-extension-xht=vivaldi.desktop;
   '';
 }
