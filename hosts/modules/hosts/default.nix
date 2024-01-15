e6c38416ff85969d2f1c096dde27aaf58be4169b
bashfulrobot <dustin@bashfulrobot.com>
2024-01-14 23:30:59 -0800

    feat: Remove unused entries from `extraHosts` section
    - Removed unnecessary entries from the `extraHosts` section in `default.nix`
    - Cleaned up and organized `default.nix` file
    
    [hosts/modules/hosts/default.nix]
    - Remove the entries `jf.lo` and `fj.lo` from the `extraHosts` section in `hosts/modules/hosts/default.nix`
    - Remove the entry `jellyfin` from the `extraHosts` section in `hosts/modules/hosts/default.nix`
    - Remove the entry `rick` from the `extraHosts` section in `hosts/modules/hosts/default.nix`
    - Remove the entry `morty` from the `extraHosts` section in `hosts/modules/hosts/default.nix`
    - Remove the entry `summer` from the `extraHosts` section in `hosts/modules/hosts/default.nix`

--- hosts/modules/hosts/default.nix
+++ hosts/modules/hosts/default.nix
@@ -1,11 +1,11 @@
-{ config, pkgs, ... }: {
+{ config, ... }: {
   # Network CFG
   networking = {
     extraHosts = ''
-      192.168.168.10 nixdo docker jellyfin
+      192.168.168.10 nixdo docker
       192.168.169.2 rembot dt
       192.168.169.3 dustin-krysak lt
-      192.168.168.1 srv
+      192.168.168.1 srv jf.lo fj.lo
       192.168.169.24 rick multiverse
       192.168.169.25 morty
       192.168.169.26 summer
