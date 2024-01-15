aa193a4cff30f188002414fc066aa7f060b7eed7
bashfulrobot <dustin@bashfulrobot.com>
2024-01-14 23:50:17 -0800

    chore: Update IP addresses for `jf.lo` and `fj.lo`
    - Modify the IP address for `jf.lo` and `fj.lo` from `192.168.168.1` to `jf.srvrs.co` and `fj.srvrs.co`
    
    [hosts/modules/hosts/default.nix]
    - Modify the IP address for `jf.lo` and `fj.lo` from `192.168.168.1` to `jf.srvrs.co` and `fj.srvrs.co`

--- hosts/modules/hosts/default.nix
+++ hosts/modules/hosts/default.nix
@@ -5,10 +5,7 @@
       192.168.168.10 nixdo docker
       192.168.169.2 rembot dt
       192.168.169.3 dustin-krysak lt
-      192.168.168.1 srv jf.lo fj.lo
-      192.168.169.24 rick multiverse
-      192.168.169.25 morty
-      192.168.169.26 summer
+      192.168.168.1 srv jf.srvrs.co fj.srvrs.co
 
     '';
   };
