4ac898b054f3de7e9f9b87f8177631334d6d9e1f
bashfulrobot <dustin@bashfulrobot.com>
2024-01-17 12:43:33 -0800

    chore: update satty command Changes to be committed: 	modified:   home/modules/scripts/default.nix     - removed output file from satty command flag

--- home/modules/scripts/default.nix
+++ home/modules/scripts/default.nix
@@ -18,7 +18,8 @@ let
     # Load the screenshot into the satty tool
     # satty --filename "$recent_screenshot"
 
-    satty --filename "$recent_screenshot" --init-tool arrow --early-exit --fullscreen --output-filename "$dir/annotated-$(date +%Y-%m-%d-%H-%M-%S).png"
+    #satty --filename "$recent_screenshot" --init-tool arrow --early-exit --fullscreen --output-filename "$dir/annotated-$(date +%Y-%m-%d-%H-%M-%S).png"
+    satty --filename "$recent_screenshot" --init-tool arrow --early-exit --fullscreen
 
     # Delete files older than 1 week
     find "$dir" -type f -mtime +7 -exec rm {} \;
