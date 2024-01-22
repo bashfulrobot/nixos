0a54b5f7296163f3b93d36c5716733de172d2958
bashfulrobot <dustin@bashfulrobot.com>
2024-01-22 10:22:21 -0800

    feature: add rcone and tools
    - added: rclone, rclone-browser, syncrclone packaged
    Changes to be committed:
    modified:   hosts/modules/pkgs/default.nix

--- hosts/modules/pkgs/default.nix
+++ hosts/modules/pkgs/default.nix
@@ -5,6 +5,8 @@
   environment.systemPackages = with pkgs; [
 
     ### Terminal
+    rclone # Command line program to sync files and directories to and from major cloud storage
+    syncrclone # Bidirectional sync tool for rclone
     textsnatcher # copy text from images
     pup # Terminal HTML parser
     libxslt # A C library and tools to do XSL transformations - needed in my terraform scripts
@@ -83,6 +85,7 @@
     gitnuro # git client
     sftpman # SftpMan consists of a CLI and a GTK application that make it simpler to setup and mount SSHFS/SFTP file systems.
     jetbrains.goland # Go IDE
+    rclone-browser # Graphical Frontend to Rclone written in Qt
     # sublime4 # code editor
     #sublime-merge # GUI Git Client
     # Causes:error: Package ‘python-2.7.18.6’ in /nix/store/zb2695k9v4hmjdki97p0yhd4mys270fs-source/pkgs/development/interpreters/python/cpython/2.7/default.nix:330 is marked as insecure, refusing to evaluate.
