3b7d180e54220eee8fe251e560a066958641c501
bashfulrobot <dustin@bashfulrobot.com>
2024-01-30 12:32:04 -0800

    chore: set juction as primary web browser Changes to be committed: 	modified:   home/modules/environment/default-apps.nix

--- home/modules/environment/default-apps.nix
+++ home/modules/environment/default-apps.nix
@@ -1,39 +1,39 @@
 { config, ... }: {
   home.file.".config/mimeapps.list".text = ''
     [Default Applications]
-    x-scheme-handler/http=microsoft-edge.desktop
-    x-scheme-handler/https=microsoft-edge.desktop
-    text/html=microsoft-edge.desktop
-    application/xhtml+xml=microsoft-edge.desktop
-    x-scheme-handler/about=microsoft-edge.desktop
-    x-scheme-handler/unknown=microsoft-edge.desktop
-    x-scheme-handler/webcal=microsoft-edge.desktop
+    x-scheme-handler/http=re.sonny.Junction.desktop
+    x-scheme-handler/https=re.sonny.Junction.desktop
+    text/html=re.sonny.Junction.desktop
+    application/xhtml+xml=re.sonny.Junction.desktop
+    x-scheme-handler/about=re.sonny.Junction.desktop
+    x-scheme-handler/unknown=re.sonny.Junction.desktop
+    x-scheme-handler/webcal=re.sonny.Junction.desktop
     text/markdown=code.desktop
     x-scheme-handler/tootle=com.github.bleakgrey.tootle.desktop
     application/x-yaml=code.desktop
-    x-scheme-handler/mailto=microsoft-edge.desktop
+    x-scheme-handler/mailto=re.sonny.Junction.desktop
     text/plain=code.desktop
     text/x-yaml=code.desktop
     application/x-shellscript=code.desktop
-    x-scheme-handler/chrome=microsoft-edge.desktop
-    application/x-extension-htm=microsoft-edge.desktop
-    application/x-extension-html=microsoft-edge.desktop
-    application/x-extension-shtml=microsoft-edge.desktop
-    application/x-extension-xhtml=microsoft-edge.desktop
-    application/x-extension-xht=microsoft-edge.desktop
+    x-scheme-handler/chrome=re.sonny.Junction.desktop
+    application/x-extension-htm=re.sonny.Junction.desktop
+    application/x-extension-html=re.sonny.Junction.desktop
+    application/x-extension-shtml=re.sonny.Junction.desktop
+    application/x-extension-xhtml=re.sonny.Junction.desktop
+    application/x-extension-xht=re.sonny.Junction.desktop
     application/pdf=org.gnome.Evince.desktop
 
     [Added Associations]
-    application/xhtml+xml=microsoft-edge.desktop
+    application/xhtml+xml=re.sonny.Junction.desktop
     text/markdown=code.desktop
     application/x-yaml=code.desktop
     text/plain=code.desktop
     application/x-shellscript=code.desktop
-    x-scheme-handler/chrome=microsoft-edge.desktop
-    application/x-extension-htm=microsoft-edge.desktop
-    application/x-extension-html=microsoft-edge.desktop
-    application/x-extension-shtml=microsoft-edge.desktop
-    application/x-extension-xhtml=microsoft-edge.desktop
-    application/x-extension-xht=microsoft-edge.desktop
+    x-scheme-handler/chrome=re.sonny.Junction.desktop
+    application/x-extension-htm=re.sonny.Junction.desktop
+    application/x-extension-html=re.sonny.Junction.desktop
+    application/x-extension-shtml=re.sonny.Junction.desktop
+    application/x-extension-xhtml=re.sonny.Junction.desktop
+    application/x-extension-xht=re.sonny.Junction.desktop
   '';
 }
