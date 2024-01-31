f1602f6917b07f4f1d2cb5088926bb9ae9b592b4
bashfulrobot <dustin@bashfulrobot.com>
2024-01-31 10:00:49 -0800

    chore: update mimelist for linkr Changes to be committed: 	modified:   home/modules/environment/default-apps.nix

--- home/modules/environment/default-apps.nix
+++ home/modules/environment/default-apps.nix
@@ -1,39 +1,39 @@
 { config, ... }: {
   home.file.".config/mimeapps.list".text = ''
     [Default Applications]
-    x-scheme-handler/http=re.sonny.Junction.desktop
-    x-scheme-handler/https=re.sonny.Junction.desktop
-    text/html=re.sonny.Junction.desktop
-    application/xhtml+xml=re.sonny.Junction.desktop
-    x-scheme-handler/about=re.sonny.Junction.desktop
-    x-scheme-handler/unknown=re.sonny.Junction.desktop
-    x-scheme-handler/webcal=re.sonny.Junction.desktop
+    x-scheme-handler/http=linkr.desktop
+    x-scheme-handler/https=linkr.desktop
+    text/html=linkr.desktop
+    application/xhtml+xml=linkr.desktop
+    x-scheme-handler/about=linkr.desktop
+    x-scheme-handler/unknown=linkr.desktop
+    x-scheme-handler/webcal=linkr.desktop
     text/markdown=code.desktop
     x-scheme-handler/tootle=com.github.bleakgrey.tootle.desktop
     application/x-yaml=code.desktop
-    x-scheme-handler/mailto=re.sonny.Junction.desktop
+    x-scheme-handler/mailto=linkr.desktop
     text/plain=code.desktop
     text/x-yaml=code.desktop
     application/x-shellscript=code.desktop
-    x-scheme-handler/chrome=re.sonny.Junction.desktop
-    application/x-extension-htm=re.sonny.Junction.desktop
-    application/x-extension-html=re.sonny.Junction.desktop
-    application/x-extension-shtml=re.sonny.Junction.desktop
-    application/x-extension-xhtml=re.sonny.Junction.desktop
-    application/x-extension-xht=re.sonny.Junction.desktop
+    x-scheme-handler/chrome=linkr.desktop
+    application/x-extension-htm=linkr.desktop
+    application/x-extension-html=linkr.desktop
+    application/x-extension-shtml=linkr.desktop
+    application/x-extension-xhtml=linkr.desktop
+    application/x-extension-xht=linkr.desktop
     application/pdf=org.gnome.Evince.desktop
 
     [Added Associations]
-    application/xhtml+xml=re.sonny.Junction.desktop
+    application/xhtml+xml=linkr.desktop
     text/markdown=code.desktop
     application/x-yaml=code.desktop
     text/plain=code.desktop
     application/x-shellscript=code.desktop
-    x-scheme-handler/chrome=re.sonny.Junction.desktop
-    application/x-extension-htm=re.sonny.Junction.desktop
-    application/x-extension-html=re.sonny.Junction.desktop
-    application/x-extension-shtml=re.sonny.Junction.desktop
-    application/x-extension-xhtml=re.sonny.Junction.desktop
-    application/x-extension-xht=re.sonny.Junction.desktop
+    x-scheme-handler/chrome=linkr.desktop
+    application/x-extension-htm=linkr.desktop
+    application/x-extension-html=linkr.desktop
+    application/x-extension-shtml=linkr.desktop
+    application/x-extension-xhtml=linkr.desktop
+    application/x-extension-xht=linkr.desktop
   '';
 }
