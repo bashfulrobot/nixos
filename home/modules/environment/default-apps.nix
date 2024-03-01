993f4d0c36d1318e489d834884355fabbaa2c581
bashfulrobot <dustin@bashfulrobot.com>
2024-02-29 17:40:07 -0800

    Set default browser from linkr for now Changes to be committed: 	modified:   home/modules/environment/default-apps.nix

--- home/modules/environment/default-apps.nix
+++ home/modules/environment/default-apps.nix
@@ -1,26 +1,26 @@
 { config, ... }: {
   home.file.".config/mimeapps.list".text = ''
     [Default Applications]
-    x-scheme-handler/http=linkr.desktop
-    x-scheme-handler/https=linkr.desktop
-    text/html=linkr.desktop
-    application/xhtml+xml=linkr.desktop
-    x-scheme-handler/about=linkr.desktop
-    x-scheme-handler/unknown=linkr.desktop
-    x-scheme-handler/webcal=linkr.desktop
+    x-scheme-handler/http=chromium.desktop
+    x-scheme-handler/https=chromium.desktop
+    text/html=chromium.desktop
+    application/xhtml+xml=chromium.desktop
+    x-scheme-handler/about=chromium.desktop
+    x-scheme-handler/unknown=chromium.desktop
+    x-scheme-handler/webcal=chromium.desktop
     text/markdown=code.desktop
     x-scheme-handler/tootle=com.github.bleakgrey.tootle.desktop
     application/x-yaml=code.desktop
-    x-scheme-handler/mailto=linkr.desktop
+    x-scheme-handler/mailto=chromium.desktop
     text/plain=code.desktop
     text/x-yaml=code.desktop
     application/x-shellscript=code.desktop
     x-scheme-handler/chrome=brave-browser.desktop
-    application/x-extension-htm=linkr.desktop
-    application/x-extension-html=linkr.desktop
-    application/x-extension-shtml=linkr.desktop
-    application/x-extension-xhtml=linkr.desktop
-    application/x-extension-xht=linkr.desktop
+    application/x-extension-htm=chromium.desktop
+    application/x-extension-html=chromium.desktop
+    application/x-extension-shtml=chromium.desktop
+    application/x-extension-xhtml=chromium.desktop
+    application/x-extension-xht=chromium.desktop
     application/pdf=org.gnome.Evince.desktop
 
     [Added Associations]
