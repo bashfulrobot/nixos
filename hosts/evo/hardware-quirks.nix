d51c085b1d3799136aca272fa487c518457c3fd0
bashfulrobot <dustin@bashfulrobot.com>
2024-03-06 09:26:29 -0800

    Remove unnecessary imports & configure NVIDIA GPU
    - Remove unnecessary imports in hosts/evo/hardware-quirks.nix
    - Add a comment about Nvidia GPU configuration
    - Modify package and settings in hosts/evo/nvidia.nix for better performance and compatibility
    
    [hosts/evo/hardware-quirks.nix]
    - Updated comments about config
    [hosts/evo/nvidia.nix]
    - Add `vaapiVdpau` package as an extra package for OpenGL
    - Enable `modesetting` for NVIDIA to make Wayland compositors function properly
    - Disable `powerManagement` and `powerManagement.finegrained` for NVIDIA
    - Set `open` to false for NVIDIA open source kernel module
    - Enable `nvidiaSettings` for accessing the NVIDIA settings menu
    - Select the appropriate driver version for the specific GPU
    - Enable `prime`, `prime.offload`, and `prime.offload.enableOffloadCmd` for NVIDIA offloading
    - Set `intelBusId` to "PCI:0:2:0" for the Intel GPU
    - Set `nvidiaBusId` to "PCI:1:0:0" for the NVIDIA GPU

--- hosts/evo/hardware-quirks.nix
+++ hosts/evo/hardware-quirks.nix
@@ -2,24 +2,19 @@
 
 {
   imports = [
-    # inputs.disko.nixosModules.disko
-    # (import ./disks.nix { })
-     # in hw
     ./fingerprint.nix
-     # in hw
     ./wifi.nix
     ./nvidia.nix
-    # in hw
     inputs.nixos-hardware.nixosModules.common-cpu-intel
-
-    inputs.nixos-hardware.nixosModules.common-gpu-nvidia
     inputs.nixos-hardware.nixosModules.common-gpu-intel
     inputs.nixos-hardware.nixosModules.common-hidpi
     inputs.nixos-hardware.nixosModules.common-pc
-     # in hw
     inputs.nixos-hardware.nixosModules.common-pc-laptop-ssd
-     # in hw
     inputs.nixos-hardware.nixosModules.common-pc-laptop
+
+    ### Nvidia GPU
+    # I pulled the config from here, and the wiki, and made my specific changes
+    # inputs.nixos-hardware.nixosModules.common-gpu-nvidia
 
   ];
 
--- hosts/evo/nvidia.nix
+++ hosts/evo/nvidia.nix
@@ -1,60 +1,63 @@
-{ config, lib, pkgs, ... }: {
+{ lib, pkgs, ... }:
 
-  # Enable OpenGL
-  hardware.opengl = {
-    enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
-  };
-
+{
   # NVIDIA drivers are unfree.
   nixpkgs.config.allowUnfree = pkgs.lib.mkForce true;
-
-  # Load nvidia driver for Xorg and Wayland
-  services.xserver.videoDrivers = [ "nvidia" ]; # or "nvidiaLegacy470 etc.
-
-  hardware.nvidia = {
-
-    # Modesetting is required.
-    modesetting.enable = true;
-
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    # Enable this if you have graphical corruption issues or application crashes after waking
-    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
-    # of just the bare essentials.
-    powerManagement.enable = false;
-
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = true;
-
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of
-    # supported GPUs is at:
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
-    # Only available from driver 515.43.04+
-    # Currently alpha-quality/buggy, so false is currently the recommended setting.
-    open = false;
-
-    # Enable the Nvidia settings menu,
-    # accessible via `nvidia-settings`.
-    nvidiaSettings = true;
-
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-
-    prime = {
-
-      sync.enable = true;
-      # enable = true;
-      # enableOffloadCmd = true;
-
-      # Bus ID of the Intel GPU. You can find it using lspci, either under 3D or VGA
-      intelBusId = "PCI:0:2:0";
-
-      # Bus ID of the NVIDIA GPU. You can find it using lspci, either under 3D or VGA
-      nvidiaBusId = "PCI:1:0:0";
+  # Sets the default video driver for the X server and Wayland to "nvidia"
+  services.xserver.videoDrivers = lib.mkDefault [ "nvidia" ];
+  hardware = {
+    opengl = {
+      # Enables the graphics driver for OpenGL
+      enable = true;
+      # Enables Direct Rendering Infrastructure (DRI), which allows the graphics driver to directly render graphics, improving performance in OpenGL
+      driSupport = true;
+      # Enables 32-bit Direct Rendering Infrastructure (DRI) support, which allows the graphics driver to directly render graphics in 32-bit applications using OpenGL
+      driSupport32Bit = true;
+      # Adds the 'vaapiVdpau' package to the extra packages for OpenGL
+      extraPackages = with pkgs; [ vaapiVdpau ];
+    };
+    nvidia = {
+      # Modesetting is required.
+      # Since NVIDIA does not support automatic KMS late loading, enabling DRM (Direct Rendering Manager) kernel mode setting is required to make Wayland compositors function properly, or to allow for Xorg#Rootless_Xorg.
+      modesetting.enable = true;
+      # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+      # Enable this if you have graphical corruption issues or application crashes after waking
+      # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
+      # of just the bare essentials.
+      powerManagement.enable = false;
+      # Fine-grained power management. Turns off GPU when not in use.
+      # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+      powerManagement.finegrained = false;
+      # Use the NVidia open source kernel module (not to be confused with the
+      # independent third-party "nouveau" open source driver).
+      # Support is limited to the Turing and later architectures. Full list of
+      # supported GPUs is at:
+      # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
+      # Only available from driver 515.43.04+
+      # Currently alpha-quality/buggy, so false is currently the recommended setting.
+      open = false;
+      # Enable the Nvidia settings menu,
+      # accessible via `nvidia-settings`.
+      nvidiaSettings = true;
+      # Optionally, you may need to select the appropriate driver version for your specific GPU.
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
+      prime = {
+        offload = {
+          enable = true;
+          enableOffloadCmd = true; # Provides `nvidia-offload` command.
+        };
+        # Bus ID of the Intel GPU. You can find it using lspci, either under 3D or VGA
+        intelBusId = "PCI:0:2:0";
+        # Bus ID of the NVIDIA GPU. You can find it using lspci, either under 3D or VGA
+        nvidiaBusId = "PCI:1:0:0";
+      };
     };
   };
+
 }
+
+#### NOTES
+# Get BusID:
+# lspci | rg "VGA|3D controller"
+# it will be in hexadecimal format, convert it to decimal
+# https://www.binaryhexconverter.com/hex-to-decimal-converter
