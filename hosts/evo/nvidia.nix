a205c4b3c30170abd20285852bfbbf88098b3968
bashfulrobot <dustin@bashfulrobot.com>
2024-03-06 20:14:27 -0800

    feat: Enable Nvidia drivers and configure graphics settings
    - Enable Nvidia drivers in NixOS configuration for Evo host
    - Set default video driver to "nvidia" for X server and Wayland
    - Enable OpenGL graphics driver and DRI support
    - Disable power management for Nvidia GPUs
    - Set Nvidia driver version to "550.54.14"
    - Update persistencedSha256 hash for Nvidia driver
    - Enable offload command for prime configuration
    - Update bus IDs for prime configuration
    
    [hosts/evo/nvidia.nix]
    - Enable nvidia drivers in the NixOS configuration for Evo host
    - Set the default video driver to "nvidia" for X server and Wayland
    - Enable OpenGL graphics driver and Direct Rendering Infrastructure (DRI) support
    - Disable power management for Nvidia GPUs
    - Set the Nvidia driver version to "550.54.14"
    - Update persistencedSha256 hash for Nvidia driver
    - Enable offload command for prime configuration
    - Update Intel and Nvidia bus IDs for prime configuration

--- hosts/evo/nvidia.nix
+++ hosts/evo/nvidia.nix
@@ -55,14 +55,14 @@
           hash = "sha256-eZiQQp2S/asE7MfGvfe6dA/kdCvek9SYa/FFGp24dVg=";
         };
       in config.boot.kernelPackages.nvidiaPackages.mkDriver {
-        # https://github.com/NixOS/nixpkgs/blob/979a311fbd179b86200e412a3ed266b64808df4e/pkgs/os-specific/linux/nvidia-x11/default.nix#L36 to choose driver
-        version = "550.40.07";
-        sha256_64bit = "sha256-KYk2xye37v7ZW7h+uNJM/u8fNf7KyGTZjiaU03dJpK0=";
-        sha256_aarch64 = "sha256-AV7KgRXYaQGBFl7zuRcfnTGr8rS5n13nGUIe3mJTXb4=";
-        openSha256 = "sha256-mRUTEWVsbjq+psVe+kAT6MjyZuLkG2yRDxCMvDJRL1I=";
-        settingsSha256 = "sha256-c30AQa4g4a1EHmaEu1yc05oqY01y+IusbBuq+P6rMCs=";
+        # https://github.com/NixOS/nixpkgs/blob/master/pkgs/os-specific/linux/nvidia-x11/default.nix to choose driver
+        version = "550.54.14";
+        sha256_64bit = "sha256-jEl/8c/HwxD7h1FJvDD6pP0m0iN7LLps0uiweAFXz+M=";
+        sha256_aarch64 = "sha256-sProBhYziFwk9rDAR2SbRiSaO7RMrf+/ZYryj4BkLB0=";
+        openSha256 = "sha256-F+9MWtpIQTF18F2CftCJxQ6WwpA8BVmRGEq3FhHLuYw=";
+        settingsSha256 = "sha256-m2rNASJp0i0Ez2OuqL+JpgEF0Yd8sYVCyrOoo/ln2a4=";
         persistencedSha256 =
-          "sha256-11tLSY8uUIl4X/roNnxf5yS2PQvHvoNjnd2CB67e870=";
+          "sha256-XaPN8jVTjdag9frLPgBtqvO/goB5zxeGzaTU0CdL6C4=";
 
         patches = [ rcu_patch ];
       };
