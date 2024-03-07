7e7a29614095b3928ddc45e4db18d9167fed013b
bashfulrobot <dustin@bashfulrobot.com>
2024-03-06 20:10:29 -0800

    feat: Add support for NVIDIA drivers and configure OpenGL and DRI settings
    - Set `nixpkgs.config.allowUnfree` to `true` to allow unfree NVIDIA drivers
    - Set `services.xserver.videoDrivers` to `[ "nvidia" ]` to set the default video driver to NVIDIA
    - Enable OpenGL graphics driver and Direct Rendering Infrastructure (DRI) support
    - Enable 32-bit DRI support for OpenGL applications
    - Set `open` to false for the Nvidia open source kernel module
    - Enable the Nvidia settings menu
    - Specify driver package details, including version and SHA256 hashes
    - Include `rcu_patch` in the driver package
    - Enable Nvidia prime offload with the Intel and Nvidia bus IDs
    
    [hosts/evo/nvidia.nix]
    - `nixpkgs.config.allowUnfree` was set to `true` to allow unfree NVIDIA drivers
    - `services.xserver.videoDrivers` was set to `[ "nvidia" ]` to set the default video driver to NVIDIA
    - `hardware.opengl.enable` was set to `true` to enable the graphics driver for OpenGL
    - `hardware.opengl.driSupport` was set to `true` to enable Direct Rendering Infrastructure (DRI) for improved performance in OpenGL
    - `hardware.opengl.driSupport32Bit` was set to `true` to enable 32-bit DRI support for OpenGL applications
    - `open` was set to `false` to use the NVIDIA open source kernel module
    - `nvidiaSettings` was set to `true` to enable the Nvidia settings menu
    - `package` was commented out to load the latest 535 driver for support of the 4070 SUPER
    - `package` was redefined to load a specific driver version (550.40.07) for the NVIDIA GPU
    - `version`, `sha256_64bit`, `sha256_aarch64`, `openSha256`, `settingsSha256`, and `persistencedSha256` were specified for the driver package
    - `patches` was defined to include the `rcu_patch` for the driver package
    - `prime.offload.enable` was set to `true` to enable offloading with the NVIDIA GPU
    - `prime.offload.enableOffloadCmd` was set to `true` to provide the `nvidia-offload` command
    - `intelBusId` was set to "PCI:0:0:2" to specify the Bus ID of the Intel GPU
    - `nvidiaBusId` was set to "PCI:0:1:0" to specify the Bus ID of the NVIDIA GPU.
    [hosts/evo/nvidia-orig.nix]
    - Add a new file `nvidia-orig.nix` to the `hosts/evo` directory
    - Enable NVIDIA drivers and set the default video driver to "nvidia"
    - Enable OpenGL graphics driver and Direct Rendering Infrastructure (DRI) support
    - Disable power management for NVIDIA and fine-grained power management
    - Set `open` to false for the Nvidia open source kernel module
    - Enable the Nvidia settings menu
    - Use the `config.boot.kernelPackages.nvidiaPackages.production` package for the Nvidia driver
    - Add a special config to load the latest 535 driver for support of the 4070 SUPER
    - Configure Nvidia prime offload with the Intel and Nvidia bus IDs

--- hosts/evo/nvidia-orig.nix
+++ hosts/evo/nvidia-orig.nix
@@ -45,27 +45,26 @@
 
       # Optionally, you may need to select the appropriate driver version for your specific GPU.
       # Note: As of early March 2024 the production driver has been updated from 535 to 550. This is a breaking change for some people, especially those on Wayland.
-      # package = config.boot.kernelPackages.nvidiaPackages.production;
+      package = config.boot.kernelPackages.nvidiaPackages.production;
       #### Special config to load the latest 535 driver for the support of the 4070 SUPER
       # https://github.com/NixOS/nixpkgs/issues/289292
-      package = let
-        rcu_patch = pkgs.fetchpatch {
-          url =
-            "https://github.com/gentoo/gentoo/raw/c64caf53/x11-drivers/nvidia-drivers/files/nvidia-drivers-470.223.02-gpl-pfn_valid.patch";
-          hash = "sha256-eZiQQp2S/asE7MfGvfe6dA/kdCvek9SYa/FFGp24dVg=";
-        };
-      in config.boot.kernelPackages.nvidiaPackages.mkDriver {
-        # https://github.com/NixOS/nixpkgs/blob/979a311fbd179b86200e412a3ed266b64808df4e/pkgs/os-specific/linux/nvidia-x11/default.nix#L36 to choose driver
-        version = "550.40.07";
-        sha256_64bit = "sha256-KYk2xye37v7ZW7h+uNJM/u8fNf7KyGTZjiaU03dJpK0=";
-        sha256_aarch64 = "sha256-AV7KgRXYaQGBFl7zuRcfnTGr8rS5n13nGUIe3mJTXb4=";
-        openSha256 = "sha256-mRUTEWVsbjq+psVe+kAT6MjyZuLkG2yRDxCMvDJRL1I=";
-        settingsSha256 = "sha256-c30AQa4g4a1EHmaEu1yc05oqY01y+IusbBuq+P6rMCs=";
-        persistencedSha256 =
-          "sha256-11tLSY8uUIl4X/roNnxf5yS2PQvHvoNjnd2CB67e870=";
+      # package = let
+      #   rcu_patch = pkgs.fetchpatch {
+      #     url =
+      #       "https://github.com/gentoo/gentoo/raw/c64caf53/x11-drivers/nvidia-drivers/files/nvidia-drivers-470.223.02-gpl-pfn_valid.patch";
+      #     hash = "sha256-eZiQQp2S/asE7MfGvfe6dA/kdCvek9SYa/FFGp24dVg=";
+      #   };
+      # in config.boot.kernelPackages.nvidiaPackages.mkDriver {
+      #   version = "535.154.05";
+      #   sha256_64bit = "sha256-fpUGXKprgt6SYRDxSCemGXLrEsIA6GOinp+0eGbqqJg=";
+      #   sha256_aarch64 = "sha256-G0/GiObf/BZMkzzET8HQjdIcvCSqB1uhsinro2HLK9k=";
+      #   openSha256 = "sha256-wvRdHguGLxS0mR06P5Qi++pDJBCF8pJ8hr4T8O6TJIo=";
+      #   settingsSha256 = "sha256-9wqoDEWY4I7weWW05F4igj1Gj9wjHsREFMztfEmqm10=";
+      #   persistencedSha256 =
+      #     "sha256-d0Q3Lk80JqkS1B54Mahu2yY/WocOqFFbZVBh+ToGhaE=";
 
-        patches = [ rcu_patch ];
-      };
+      #   patches = [ rcu_patch ];
+      # };
       prime = {
         offload = {
           enable = true;
