ccd200fda5e941cef30329edfa70c91770066ac9
bashfulrobot <dustin@bashfulrobot.com>
2024-03-11 12:26:31 -0700

    style: Update theme colors for alacritty and gitnuro
    - Updated the alacritty.nix file with new color values for various elements including background, foreground, cursor, selection, and text colors
    - Modified the gitnauro.nix file to change the theme colors for different UI elements including primary, secondary, error, background, surface, and scrollbar colors
    
    [home/modules/themes/yaru/apps/alacritty.nix]
    - Change the primary background color to "#2C001E" and the foreground color to "#F2F2F2"
    - Change the cursor text color to "#2C001E" and the cursor color to "#F2F2F2"
    - Change the vi mode cursor text color to "#2C001E" and the cursor color to "#F2F2F2"
    - Change the selection text color to "#F2F2F2" and the background color to "#4C2294"
    - Change the search matches foreground color to "#4C2294" and the background color to "#F2F2F2"
    - Change the focused match foreground color to "#4C2294" and the background color to "#F2F2F2"
    - Change the footer bar background color to "#2C001E" and the foreground color to "#F2F2F2"
    - Change the hints start foreground color to "#2C001E" and the background color to "#F2F2F2"
    - Change the hints end foreground color to "#F2F2F2" and the background color to "#2C001E"
    - Change the normal black color to "#2C001E", red color to "#FF5555", green color to "#50FA7B", yellow color to "#F1FA8C", blue color to "#BD93F9", magenta color to "#FF79C6", cyan color to "#8BE9FD", and white color to "#F2F2F2"
    - Change the bright black color to "#4C2294", red color to "#FF6E6E", green color to "#69FF94", yellow color to "#FFFFA5", blue color to "#D6ACFF", magenta color to "#FF92DF", cyan color to "#A4FFFF", and white color to "#FFFFFF"
    - Change the dim black color to "#2C001E", red color to "#FF5555", green color to "#50FA7B", yellow color to "#F1FA8C", blue color to "#BD93F9", magenta color to "#FF79C6", cyan color to "#8BE9FD", and white color to "#F2F2F2"
    [home/modules/themes/yaru/apps/gitnauro.nix]
    - Modify the gitnuro theme colors
    - Change the `"primary"` color value to `#2C001E`
    - Change the `"primaryVariant"` color value to `#4C2294`
    - Change the `"onPrimary"` color value to `#F2F2F2`
    - Change the `"secondary"` color value to `#50FA7B`
    - Change the `"onBackground"` color value to `#2C001E`
    - Change the `"onBackgroundSecondary"` color value to `#4C2294`
    - Change the `"error"` color value to `#FF5555`
    - Change the `"onError"` color value to `#F2F2F2`
    - Change the `"background"` color value to `#2C001E`
    - Change the `"backgroundSelected"` color value to `#4C2294`
    - Change the `"surface"` color value to `#4C2294`
    - Change the `"secondarySurface"` color value to `#4C2294`
    - Change the `"tertiarySurface"` color value to `#4C2294`
    - Change the `"addFile"` color value to `#50FA7B`
    - Change the `"deletedFile"` color value to `#FF5555`
    - Change the `"modifiedFile"` color value to `#F1FA8C`
    - Change the `"conflictingFile"` color value to `#FF6E6E`
    - Change the `"normalScrollbar"` color value to `#4C2294`
    - Change the `"hoverScrollbar"` color value to `#F2F2F2`
    - Change the `"diffLineAdded"` color value to `#50FA7B`
    - Change the `"diffLineRemoved"` color value to `#FF5555`

--- home/modules/themes/yaru/apps/alacritty.nix
+++ home/modules/themes/yaru/apps/alacritty.nix
@@ -3,43 +3,43 @@
     settings = {
       colors = {
         primary = {
-          background = "#2E3436";
-          foreground = "#D3D7CF";
+          background = "#2C001E";
+          foreground = "#F2F2F2";
         };
         cursor = {
-          text = "#2E3436";
-          cursor = "#D3D7CF";
+          text = "#2C001E";
+          cursor = "#F2F2F2";
         };
         vi_mode_cursor = {
-          text = "#2E3436";
-          cursor = "#D3D7CF";
+          text = "#2C001E";
+          cursor = "#F2F2F2";
         };
         selection = {
-          text = "#D3D7CF";
-          background = "#555753";
+          text = "#F2F2F2";
+          background = "#4C2294";
         };
         search = {
           matches = {
-            foreground = "#555753";
-            background = "#729FCF";
+            foreground = "#4C2294";
+            background = "#F2F2F2";
           };
           focused_match = {
-            foreground = "#555753";
-            background = "#3465A4";
+            foreground = "#4C2294";
+            background = "#F2F2F2";
           };
         };
         footer_bar = {
-          background = "#2E3436";
-          foreground = "#D3D7CF";
+          background = "#2C001E";
+          foreground = "#F2F2F2";
         };
         hints = {
           start = {
-            foreground = "#2E3436";
-            background = "#729FCF";
+            foreground = "#2C001E";
+            background = "#F2F2F2";
           };
           end = {
-            foreground = "#729FCF";
-            background = "#2E3436";
+            foreground = "#F2F2F2";
+            background = "#2C001E";
           };
         };
         line_indicator = {
@@ -47,34 +47,34 @@
           background = "None";
         };
         normal = {
-          black = "#2E3436";
-          red = "#CC0000";
-          green = "#4E9A06";
-          yellow = "#C4A000";
-          blue = "#3465A4";
-          magenta = "#75507B";
-          cyan = "#06989A";
-          white = "#D3D7CF";
+          black = "#2C001E";
+          red = "#FF5555";
+          green = "#50FA7B";
+          yellow = "#F1FA8C";
+          blue = "#BD93F9";
+          magenta = "#FF79C6";
+          cyan = "#8BE9FD";
+          white = "#F2F2F2";
         };
         bright = {
-          black = "#555753";
-          red = "#EF2929";
-          green = "#8AE234";
-          yellow = "#FCE94F";
-          blue = "#729FCF";
-          magenta = "#AD7FA8";
-          cyan = "#34E2E2";
-          white = "#EEEEEC";
+          black = "#4C2294";
+          red = "#FF6E6E";
+          green = "#69FF94";
+          yellow = "#FFFFA5";
+          blue = "#D6ACFF";
+          magenta = "#FF92DF";
+          cyan = "#A4FFFF";
+          white = "#FFFFFF";
         };
         dim = {
-          black = "#2E3436";
-          red = "#CC0000";
-          green = "#4E9A06";
-          yellow = "#C4A000";
-          blue = "#3465A4";
-          magenta = "#75507B";
-          cyan = "#06989A";
-          white = "#D3D7CF";
+          black = "#2C001E";
+          red = "#FF5555";
+          green = "#50FA7B";
+          yellow = "#F1FA8C";
+          blue = "#BD93F9";
+          magenta = "#FF79C6";
+          cyan = "#8BE9FD";
+          white = "#F2F2F2";
         };
       };
     };
