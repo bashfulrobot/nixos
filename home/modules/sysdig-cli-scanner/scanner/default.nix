cb684eeb35a4d2794b3dd0cd0bd52e149d01bc68
bashfulrobot <dustin@bashfulrobot.com>
2024-03-12 16:35:05 -0700

    chore: Update the sha256 value for the `sysdig-cli-scanner` source in `default.nix`
    - Update sha256 value for `sysdig-cli-scanner` source in `default.nix`
    
    [home/modules/sysdig-cli-scanner/scanner/default.nix]
    - Update the sha256 value for the `sysdig-cli-scanner` source in `default.nix`

--- home/modules/sysdig-cli-scanner/scanner/default.nix
+++ home/modules/sysdig-cli-scanner/scanner/default.nix
@@ -6,7 +6,7 @@ in pkgs.stdenv.mkDerivation {
   src = pkgs.fetchurl {
     url =
       "https://download.sysdig.com/scanning/bin/sysdig-cli-scanner/${version}/linux/amd64/sysdig-cli-scanner";
-    sha256 = "sha256-XYXV+X5dEUm6+ZYnbpZY8x++5jrhup1spmRM46EipVI=";
+    sha256 = "sha256-mU9D0PGUIUV7JGnDrVG2EGeWb38MIzZ9RRbi+SqMjBo=";
   };
   phases = [ "installPhase" "patchPhase" ];
   installPhase = ''
