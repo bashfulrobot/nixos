494d1c9431a79613aa9f497d63399f03dca811d0
bashfulrobot <dustin@bashfulrobot.com>
2024-03-12 10:05:58 -0700

    Add Kolide configuration and enable experimental features
    - Add `../modules/kolide` import in `default.nix` file
    - Enable experimental features for `nix-command` and flakes in `kolide/default.nix`
    - Update various configurations in `kolide/default.nix` file
    - Add `reference.nix` file to `kolide` module directory
    - Configure `imports` to include `google-compute-image.nix` in `kolide/reference.nix`
    - Add packages (`git`, `vim`, `google-cloud-sdk-gce`, `inputs.kolide-launcher`) to systemPackages in `kolide/reference.nix`
    - Enable `kolide-launcher` service with specific configurations in `kolide/reference.nix`
    - Set `EDITOR` environment variable to `vim` in `kolide/reference.nix`
    - Allow unfree packages in `nixpkgs.config` in `kolide/reference.nix`
    
    [hosts/evo/default.nix]
    - Add `../modules/kolide` to the list of imports
    [hosts/modules/kolide/default.nix]
    - Enable experimental features for nix-command and flakes
    - Allow unfree packages in nixpkgs
    - Remove unnecessary comments about installing packages
    - Enable the kolide-launcher service
    - Set the kolideHostname to "dustin-krysak"
    - Set the rootDirectory to "/var/kolide-k2/dustin-krysak"
    - Set the updateChannel to "nightly"
    - Set the autoupdateInterval to "2m"
    - Set the autoupdaterInitialDelay to "1m"
    [hosts/modules/kolide/reference.nix]
    - Add `reference.nix` file to the `hosts/modules/kolide` directory
    - Configure `imports` to include `google-compute-image.nix` from `inputs.nixpkgs`
    - Add `git`, `vim`, `google-cloud-sdk-gce`, and `inputs.kolide-launcher` to `environment.systemPackages`
    - Enable `kolide-launcher` service with specific configurations
    - Set `EDITOR` environment variable to `"vim"`
    - Allow unfree packages in `nixpkgs.config`

--- hosts/modules/kolide/default.nix
+++ hosts/modules/kolide/default.nix
@@ -1,19 +1,23 @@
 { pkgs, config, inputs, lib, ... }: {
 
-  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [ "kolide-launcher-1.5.3" ];
-  # Install necessary packages
-  # environment.systemPackages = [
-  #   inputs.kolide-launcher
-  # ];
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nixpkgs.config.allowUnfree = true;
 
-  environment.etc."kolide-k2/secret" = {
-    mode = "0600";
-    text = "";
-  };
+  # nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [ "kolide-launcher-1.5.3" ];
+  # Install necessary packages
+  environment.systemPackages = [ inputs.kolide-launcher ];
+
+  # environment.etc."kolide-k2/secret" = {
+  #   mode = "0600";
+  #   text = "";
+  # };
 
   services.kolide-launcher = {
     enable = true;
     kolideHostname = "dustin-krysak";
-    # updateChannel = "nightly";
+    rootDirectory = "/var/kolide-k2/dustin-krysak";
+    updateChannel = "nightly";
+    autoupdateInterval = "2m";
+    autoupdaterInitialDelay = "1m";
   };
 }
