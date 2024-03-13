9adfe8a43be4ce640e7509c207fc4600949d10ba
bashfulrobot <dustin@bashfulrobot.com>
2024-03-13 09:15:15 -0700

    bug: Update default applications for Chromium
    - Updated default applications in `home/modules/environment/default-apps.nix`
    - Changed `chromium.desktop` to `chromium-browser.desktop` for various default applications
    - Added `chromium-browser.desktop` as the default application for several schemes and extensions
    
    [home/modules/environment/default-apps.nix]
    - Change `chromium.desktop` to `chromium-browser.desktop` in the `text/html` default application
    - Change `chromium.desktop` to `chromium-browser.desktop` in the `x-scheme-handler/http` default application
    - Change `chromium.desktop` to `chromium-browser.desktop` in the `x-scheme-handler/https` default application
    - Add `chromium-browser.desktop` as the default application for `x-scheme-handler/about`
    - Add `chromium-browser.desktop` as the default application for `x-scheme-handler/unknown`
    - Add `chromium-browser.desktop` as the default application for `x-scheme-handler/webcal`
    - Change `chromium.desktop` to `chromium-browser.desktop` in the `x-scheme-handler/mailto` default application
    - Change `chromium.desktop` to `chromium-browser.desktop` in the `application/x-extension-htm` default application
    - Change `chromium.desktop` to `chromium-browser.desktop` in the `application/x-extension-html` default application
    - Change `chromium.desktop` to `chromium-browser.desktop` in the `application/x-extension-shtml` default application
    - Change `chromium.desktop` to `chromium-browser.desktop` in the `application/x-extension-xhtml` default application
    - Change `chromium.desktop` to `chromium-browser.desktop` in the `application/x-extension-xht` default application

--- home/modules/environment/default-apps.nix
+++ home/modules/environment/default-apps.nix
@@ -1,26 +1,34 @@
 { config, ... }: {
   home.file.".config/mimeapps.list".text = ''
+
     [Default Applications]
-    x-scheme-handler/http=chromium.desktop
-    x-scheme-handler/https=chromium.desktop
-    text/html=chromium.desktop
-    application/xhtml+xml=chromium.desktop
-    x-scheme-handler/about=chromium.desktop
-    x-scheme-handler/unknown=chromium.desktop
-    x-scheme-handler/webcal=chromium.desktop
+    text/html=chromium-browser.desktop
+    x-scheme-handler/http=chromium-browser.desktop
+    x-scheme-handler/https=chromium-browser.desktop
+    x-scheme-handler/about=chromium-browser.desktop
+    x-scheme-handler/unknown=chromium-browser.desktop
+
+    [Default Applications]
+    x-scheme-handler/http=chromium-browser.desktop
+    x-scheme-handler/https=chromium-browser.desktop
+    text/html=chromium-browser.desktop
+    application/xhtml+xml=chromium-browser.desktop
+    x-scheme-handler/about=chromium-browser.desktop
+    x-scheme-handler/unknown=chromium-browser.desktop
+    x-scheme-handler/webcal=chromium-browser.desktop
     text/markdown=code.desktop
     x-scheme-handler/tootle=com.github.bleakgrey.tootle.desktop
     application/x-yaml=code.desktop
-    x-scheme-handler/mailto=chromium.desktop
+    x-scheme-handler/mailto=chromium-browser.desktop
     text/plain=code.desktop
     text/x-yaml=code.desktop
     application/x-shellscript=code.desktop
     x-scheme-handler/chrome=brave-browser.desktop
-    application/x-extension-htm=chromium.desktop
-    application/x-extension-html=chromium.desktop
-    application/x-extension-shtml=chromium.desktop
-    application/x-extension-xhtml=chromium.desktop
-    application/x-extension-xht=chromium.desktop
+    application/x-extension-htm=chromium-browser.desktop
+    application/x-extension-html=chromium-browser.desktop
+    application/x-extension-shtml=chromium-browser.desktop
+    application/x-extension-xhtml=chromium-browser.desktop
+    application/x-extension-xht=chromium-browser.desktop
     application/pdf=org.gnome.Evince.desktop
 
     [Added Associations]
