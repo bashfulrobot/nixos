6b83d592c4a4d73601405494553a66b8ba84e1df
bashfulrobot <dustin@bashfulrobot.com>
2024-03-21 14:23:35 -0700

    feat: Update Kolide configuration and enable launcher service
    - Removed comment surrounding `environment.etc."kolide-k2/secret"` in `hosts/modules/kolide/default.nix`
    - Added empty string value to `environment.etc."kolide-k2/secret"`
    - Enabled `services.kolide-launcher` service
    - Updated `kolideHostname` value to `my-kolide-hostname`
    
    [hosts/modules/kolide/default.nix]
    - Remove the comment that was surrounding `environment.etc."kolide-k2/secret" = { ... };`
    - Add a text value to `environment.etc."kolide-k2/secret" = { ... };` and set it to an empty string
    - Enable the `services.kolide-launcher` service
    - Change the value of `kolideHostname` from `dustin-krysak` to `my-kolide-hostname`

--- hosts/modules/kolide/default.nix
+++ hosts/modules/kolide/default.nix
@@ -3,14 +3,13 @@
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   nixpkgs.config.allowUnfree = true;
 
-  # nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [ "kolide-launcher-1.5.3" ];
   # Install necessary packages
   environment.systemPackages = [ inputs.kolide-launcher ];
 
-  # environment.etc."kolide-k2/secret" = {
-  #   mode = "0600";
-  #   text = "";
-  # };
+  environment.etc."kolide-k2/secret" = {
+    mode = "0600";
+    text = "";
+  };
 
   services.kolide-launcher = {
     enable = true;
