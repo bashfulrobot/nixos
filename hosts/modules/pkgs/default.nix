2b6ac59d8651391bec6b392c957e2b68730a063e
bashfulrobot <dustin@bashfulrobot.com>
2024-04-04 15:37:17 -0700

    chore: Update default packages: add new packages and remove obsolete ones
    - Clean up unused editors
    
    [hosts/modules/pkgs/default.nix]
    - Remove the package `gnome-builder`
    - Remove the package `helix`

--- hosts/modules/pkgs/default.nix
+++ hosts/modules/pkgs/default.nix
@@ -92,8 +92,6 @@
     nodejs-18_x # JavaScript runtime
     # postman # API testing tool
     vscode # code editor
-    gnome-builder # IDE
-		helix # code editor
     sftpman # SftpMan consists of a CLI and a GTK application that make it simpler to setup and mount SSHFS/SFTP file systems.
     # jetbrains.goland # Go IDE
     rclone-browser # Graphical Frontend to Rclone written in Qt
