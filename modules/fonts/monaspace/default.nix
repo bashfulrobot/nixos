a794207e256571c4329bceee6d1e7221be2fafb5
bashfulrobot <dustin@bashfulrobot.com>
2024-04-22 12:04:52 -0700

    Refactor: merge home manager into config file modules. Add fonts.

---
+++ modules/fonts/monaspace/default.nix
@@ -0,0 +1,26 @@
+{ lib, pkgs, ... }:
+let version = "1.000";
+in pkgs.stdenvNoCC.mkDerivation {
+  name = "monaspace-font";
+  dontConfigue = true;
+
+  src = pkgs.fetchzip {
+    url =
+      "https://github.com/githubnext/monaspace/releases/download/v${version}/monaspace-v${version}.zip";
+    sha256 = "sha256-H8NOS+pVkrY9DofuJhPR2OlzkF4fMdmP2zfDBfrk83A=";
+    stripRoot = true;
+  };
+
+  installPhase = ''
+    # Copy commands from the official linux install script modified for nix
+    mkdir -p $out/share/fonts
+    cp $src/monaspace-v${version}/fonts/variable/* $out/share/fonts/
+    cp $src/monaspace-v${version}/fonts/otf/* $out/share/fonts/
+  '';
+
+  meta = with lib; {
+    description =
+      "A monaspace font derivation. https://github.com/githubnext/monaspace#monaspace";
+    maintainers = [ bashfulrobot ];
+  };
+}
