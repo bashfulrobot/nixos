a9da072a055479cc14b8e837f8590c1dcbe2788d
bashfulrobot <dustin@bashfulrobot.com>
2024-04-22 21:29:16 -0700

    chore: Refactor file structure for AMD GPU configurations
    - Renamed the file `modules/amd-gpu/default.nix` to `systems/rembot/hardware/amdgpu.nix`
    - Added a new import for `amdgpu.nix` file in `systems/rembot/hardware/default.nix`
    
    [modules/amd-gpu/default.nix]
    - Rename the file `modules/amd-gpu/default.nix` to `systems/rembot/hardware/amdgpu.nix`
    [systems/rembot/hardware/default.nix]
    - Added a new import for `amdgpu.nix` file.

--- systems/rembot/hardware/default.nix
+++ systems/rembot/hardware/default.nix
@@ -5,13 +5,13 @@
 
     ./boot.nix
     ./hardware-configuration.nix
+    ./amdgpu.nix
     inputs.nixos-hardware.nixosModules.common-cpu-amd
     inputs.nixos-hardware.nixosModules.common-cpu-amd-pstate
     inputs.nixos-hardware.nixosModules.common-gpu-amd
     inputs.nixos-hardware.nixosModules.common-hidpi
     inputs.nixos-hardware.nixosModules.common-pc
     inputs.nixos-hardware.nixosModules.common-pc-ssd
-    ../../../modules/amd-gpu # AMD GPU support - need to review with the above AMD config
 
   ];
 
