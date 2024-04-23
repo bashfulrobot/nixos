52e1b4b7c3aa2374cd7298ce7285d2ea3ab7a830
bashfulrobot <dustin@bashfulrobot.com>
2024-04-23 10:36:33 -0700

    feat: Add new hardware module configurations
    - Added new files for Bluetooth, WiFi, fprintd, touchpad, and audio modules
    - Enabled various configurations for Bluetooth, WiFi, and touchpad support
    - Configured sound settings with pipewire and disabled pulseaudio
    - Enabled security rtkit and added optional configuration for JACK
    - No additional system packages were added
    - Updated `modules/hardware/default.nix` with new imports
    
    [modules/hardware/bluetooth/default.nix]
    - Add `default.nix` file to modules/hardware/bluetooth directory
    - Enable Blueman Bluetooth Manager service
    - Add `hid-apple` kernel module
    - Enable Bluetooth and configure settings for high quality BT calls
    - Enable Logitech wireless functionality
    [modules/hardware/wifi/default.nix]
    - Add a new file `default.nix` to the `modules/hardware/wifi` directory
    - Include a comment about the WiFi device being slow and prone to crashes
    - Add a configuration to disable WiFi 6E and enable power save for the device
    - Include an empty list for `environment.systemPackages`
    - Add a configuration for WiFi power saving in the `boot.extraModprobeConfig` variable
    [modules/hardware/fprintd/default.nix]
    - Add a new file `modules/hardware/fprintd/default.nix`
    [modules/hardware/touchpad/default.nix]
    - Add new file `modules/hardware/touchpad/default.nix`
    - Enable touchpad support in `services.xserver.libinput`
    [modules/hardware/audio/default.nix]
    - Added a new file `modules/hardware/audio/default.nix`
    - Enabled sound with pipewire
    - Disabled pulseaudio
    - Enabled security rtkit
    - Enabled pipewire service, alsa, 32-bit support, and pulse
    - Provided an optional configuration for enabling JACK
    - Added an example session manager for media sessions
    - No additional system packages were added
    [modules/hardware/default.nix]
    - Added a new file: `modules/hardware/default.nix`
    - The file contains an object with an `imports` array

---
+++ modules/hardware/default.nix
@@ -0,0 +1,11 @@
+{ ... }: {
+
+  imports = [
+
+    ./audio
+    ./bluetooth
+    ./touchpad
+
+  ];
+
+}
---
+++ modules/hardware/touchpad/default.nix
@@ -0,0 +1,6 @@
+{ ... }: {
+
+   # Enable touchpad support (enabled default in most desktopManager).
+  services.xserver.libinput.enable = true;
+
+}
