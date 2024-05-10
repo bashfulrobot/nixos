ff06a8990e824daca5cd72f9a20b8eac99ddec68
bashfulrobot <dustin@bashfulrobot.com>
2024-05-10 00:45:12 -0700

    refactor: Remove unnecessary import and enhance file structure
    - Commented out the import of `../../../modules/workstation/steam` in `systems/rembot/config/default.nix`
    
    [systems/rembot/config/default.nix]
    - Commented out the `../../../modules/workstation/steam` import

--- systems/rembot/config/default.nix
+++ systems/rembot/config/default.nix
@@ -5,13 +5,19 @@
     ../../../modules/workstation/desktops/gnome # Desktop for this system
     ../../../modules/workstation/syncthing/rembot # Syncthing configuration
     ../../../modules/workstation/desktop-files/tower # Custom menu items
-    ../../../modules/workstation/steam # Steam configuration
+    # ../../../modules/workstation/steam # Steam configuration
     ./video # video specific configuration
     # ./kolide
+    ../../../src/autoimport.nix
 
   ];
 
-  # archetype.workstation = true;
+  archetype = {
+    core.enable = true;
+    workstation.enable = true;
+  };
+
+  suites = { tech-support.enable = true; };
 
   networking.hostName = "rembot"; # Define your hostname.
 }
