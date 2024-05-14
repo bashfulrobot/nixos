5f04a4cb1dc81ff2aad30ef35e6c8ddedbc8bd60
bashfulrobot <dustin@bashfulrobot.com>
2024-05-13 23:03:19 -0700

    feat: new avalanche desktop
    - Not yet primetime. Just a template.
    Changes to be committed:
    	new file:   modules/desktops/disabled/avalanche/default.nix

---
+++ modules/desktops/disabled/avalanche/default.nix
@@ -0,0 +1,24 @@
+# { lib, config, inputs, pkgs, ... }:
+{ pkgs, config, lib, inputs, ... }:
+let
+  cfg = config.desktops.avalanche;
+  # Used in my home manager code at the bottom of the file.
+  username = if builtins.getEnv "SUDO_USER" != "" then
+    builtins.getEnv "SUDO_USER"
+  else
+    builtins.getEnv "USER";
+in {
+  options = {
+    desktops.avalanche.enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Enable Avalanche Desktop";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    home-manager.users."${username}" = {
+      snowfallorg.avalanche.desktop.enable = true;
+    };
+  };
+}
