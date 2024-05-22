18736090caa1118cd46ea10746b8aefcd78c2f41
bashfulrobot <dustin@bashfulrobot.com>
2024-05-22 08:49:54 -0700

    feat: Add template application configuration
    [modules/apps/disabled/template-app/default.nix]
    - Add a new file: `modules/apps/disabled/template-app/default.nix`
    - Add a template for configuring applications
    - Provide an option `apps.template-app.enable` to enable or disable the application
    - Determine the username for the home-manager configuration based on the environment variables `SUDO_USER` and `USER`
    - When enabled, add the application to system packages and update home-manager configuration for the specified username
    [modules/apps/disabled/firefox/default.nix]
    - Delete the file `modules/apps/disabled/firefox/default.nix`
    - replaced with myh real FF config in another file
    
    Changes to be committed:
    	deleted:    modules/apps/disabled/firefox/default.nix
    	new file:   modules/apps/disabled/template-app/default.nix

---
+++ modules/apps/disabled/template-app/default.nix
@@ -0,0 +1,51 @@
+# =============================================================================
+# MODULE: Template Application Configuration
+# =============================================================================
+#
+# OVERVIEW
+# --------
+# This Nix file serves as a template for configuring applications.
+# It provides a placeholder `apps.template-app.enable` option to enable or disable the application.
+# Replace `template-app` with the actual name of your application.
+#
+# DETAILS
+# -------
+# When the application is enabled, it is added to the system packages.
+# The username for the home-manager configuration is determined based on the environment variables `SUDO_USER` and `USER`.
+# If `SUDO_USER` is not empty, it is used as the username; otherwise, `USER` is used.
+#
+# USAGE
+# -----
+# Use this file as a base to create configuration files for other applications.
+# Note - modules/apps/syncthing/default.nix is a good example with multiple options, and the use of `mkMerge` to combine them.
+# Without `mkMerge`, the last option will override the previous ones.
+#
+# =============================================================================
+
+{ pkgs, config, lib, ... }:
+let
+  cfg = config.apps.template-app;
+  username = if builtins.getEnv "SUDO_USER" != "" then
+    builtins.getEnv "SUDO_USER"
+  else
+    builtins.getEnv "USER";
+in {
+
+  options = {
+    apps.template-app.enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Enable the template-app application.";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+
+    # Regular nix configuration
+    environment.systemPackages = with pkgs; [ template-app ];
+
+    home-manager.users."${username}" = {
+      # Home-manager configuration
+     };
+  };
+}
