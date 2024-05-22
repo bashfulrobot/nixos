d90705831b56c5621599aacab8b640b44fb3846d
bashfulrobot <dustin@bashfulrobot.com>
2024-05-21 23:10:33 -0700

    feat: Refactor and optimize Firefox configuration in home-manager
    - Added `modules/apps/disabled/firefox/default.nix`
    - Ported the file to home-manager
    - Modified `modules/apps/firefox/default.nix` for Firefox in home-manager
    - Added logic to retrieve the username from the environment variables
    - Enabled the Firefox browser if `cfg.enable` is true
    - Modified the `package` attribute to wrap Firefox with additional policies
    - Added profiles and extensions to the Firefox configuration
    - Set session variables for Firefox
    - Deleted `modules/apps/disabled/ff-new/default.nix`
    
    [modules/apps/disabled/firefox/default.nix]
    - Add a new file `modules/apps/disabled/firefox/default.nix`
    - Port the file to home-manager
    [modules/apps/firefox/default.nix]
    - Modify the `default.nix` file for Firefox in home-manager
    - Add logic to retrieve the username from the environment variables
    - Enable the Firefox browser if `cfg.enable` is true
    - Modify the `package` attribute to wrap Firefox with additional policies
    - Add profiles and extensions to the Firefox configuration
    - Set session variables for Firefox
    [modules/apps/disabled/ff-new/default.nix]
    - The file `modules/apps/disabled/ff-new/default.nix` was deleted in this commit.

---
+++ modules/apps/disabled/firefox/default.nix
@@ -0,0 +1,19 @@
+# Port to home-manager
+{ pkgs, config, lib, ... }:
+let
+    cfg = config.apps.firefox;
+in
+ {
+
+  options = {
+    apps.firefox.enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Enable the Firefox browser.";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [ firefox ];
+  };
+}
