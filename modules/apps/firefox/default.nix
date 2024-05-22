3007ec6ed9f6b5a71fcc5c99edddd47470416732
bashfulrobot <dustin@bashfulrobot.com>
2024-05-21 23:32:27 -0700

    feat: Configure Firefox browser settings in default.nix
    - Enabled Firefox browser in the configuration
    - Enabled Pipewire support and disabled Captive Portal
    - Disabled Firefox studies and save logins offer
    - Disabled telemetry and Firefox accounts
    - Enabled NoDefaultBookmarks
    - Disabled extension recommendations and user messaging onboarding
    - Added Firefox addons to the default profile
    - Set session variables MOZ_ENABLE_WAYLAND to 1 and MOZ_USE_XINPUT2 to "1"
    
    [modules/apps/firefox/default.nix]
    - Enable the Firefox browser in the config
    - Set `pipewireSupport` to true and `CaptivePortal` to false
    - Disable Firefox studies and save logins offer
    - Disable telemetry and Firefox accounts
    - Set `NoDefaultBookmarks` to true
    - Disable extension recommendations and skip onboarding for user messaging
    - Add firefox addons to the default profile
    - Set session variables `MOZ_ENABLE_WAYLAND` to 1 and `MOZ_USE_XINPUT2` to "1"

--- modules/apps/firefox/default.nix
+++ modules/apps/firefox/default.nix
@@ -55,27 +55,28 @@ in {
             name = "Default";
             isDefault = true;
             extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+              # Currently using firefox sync
               # see: https://github.com/nix-community/nur-combined/blob/master/repos/rycee/pkgs/firefox-addons/generated-firefox-addons.nix
               # https-everywhere
               # languagetool
-              onepassword-password-manager
-              gnome-shell-integration
-              darkreader
-              grammarly
-              ublock-origin
-              dracula-dark-colorscheme
-              markdownload
-              privacy-badger
-              multi-account-containers
-              new-window-without-toolbar
-              decentraleyes
-              okta-browser-plugin
-              theater-mode-for-youtube
-              link-cleaner
-              xbrowsersync
-              redirector
-              auto-tab-discard
-              amp2html
+              # onepassword-password-manager
+              # gnome-shell-integration
+              # darkreader
+              # grammarly
+              # ublock-origin
+              # dracula-dark-colorscheme
+              # markdownload
+              # privacy-badger
+              # multi-account-containers
+              # new-window-without-toolbar
+              # decentraleyes
+              # okta-browser-plugin
+              # theater-mode-for-youtube
+              # link-cleaner
+              # # xbrowsersync
+              # redirector
+              # auto-tab-discard
+              # amp2html
             ];
             # bookmarks = {
             #   "📎 Add bookmark" = {
