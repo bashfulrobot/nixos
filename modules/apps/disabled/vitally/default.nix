37fb2a400c24142f20db054c042b6c4740a25af9
bashfulrobot <dustin@bashfulrobot.com>
2024-05-22 08:51:18 -0700

    WIP: Update URL and configuration for `programs.firefox.webapps.vitally`
    - Updated the URL for the `programs.firefox.webapps.vitally` application
    - Removed an unnecessary comment about the `programs.firefox.webapps.vitally` application
    - Added the `home-manager.users."${username}"` section for the `programs.firefox.webapps.vitally` application
    
    [modules/apps/disabled/vitally/default.nix]
    - Update the URL for the `programs.firefox.webapps.vitally` application
    - Remove the unnecessary comment about the `programs.firefox.webapps.vitally` application
    - Add the `home-manager.users."${username}"` section for the `programs.firefox.webapps.vitally` application
    
    Changes to be committed:
    	modified:   modules/apps/disabled/vitally/default.nix

--- modules/apps/disabled/vitally/default.nix
+++ modules/apps/disabled/vitally/default.nix
@@ -6,16 +6,18 @@ let
   else
     builtins.getEnv "USER";
 in {
-  programs.firefox.webapps.vitally = {
-    url = "https://sysdig.vitally.io/hubs/553ec776-875e-4a0e-a096-a3da3a0b6ea1/8acc40cb-e3ea-4da8-9570-4be27a10fff6";
-    id = 0;
+  home-manager.users."${username}" = {
+    programs.firefox.webapps.vitally = {
+      url =
+        "https://sysdig.vitally.io/hubs/553ec776-875e-4a0e-a096-a3da3a0b6ea1/8acc40cb-e3ea-4da8-9570-4be27a10fff6";
+      id = 0;
 
-    extraSettings = config.programs.firefox.profiles."${username}".settings;
-    backgroundColor = "#202225";
+      extraSettings = config.programs.firefox.profiles."${username}".settings;
+      backgroundColor = "#202225";
 
-    comment =
-      "Customer Success Dashboard.";
-    genericName = "Vitally";
-    categories = [ "Office" "Web" ];
+      comment = "Customer Success Dashboard.";
+      genericName = "Vitally";
+      categories = [ "Office" "Web" ];
+    };
   };
 }
