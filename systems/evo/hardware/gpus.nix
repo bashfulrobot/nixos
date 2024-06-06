3aa1d08516274d9e81bd25edc9eb7005bcba18e7
bashfulrobot <dustin@bashfulrobot.com>
2024-06-05 22:48:44 -0700

    fix: Fix typos and enable offload command for GPU support
    - Fix typo in `offload.enable` line in `systems/evo/hardware/gpus.nix`
    - Uncomment `offload.enableOffloadCmd` line in `systems/evo/hardware/gpus.nix`
    
    [systems/evo/hardware/gpus.nix]
    - Fix a typo in the `offload.enable` line
    - Uncomment the `offload.enableOffloadCmd` line

--- systems/evo/hardware/gpus.nix
+++ systems/evo/hardware/gpus.nix
@@ -50,8 +50,8 @@
       prime = {
         intelBusId = "PCI:0:2:0";
         nvidiaBusId = "PCI:1:0:0";
-        offload = true;
-        # offload.enableOffloadCmd = true;
+        offload.enable = true;
+        offload.enableOffloadCmd = true;
         # Make the Intel iGP default. The NVIDIA Quadro is for CUDA/NVENC
         # reverseSync.enable = true;
         # sync.enable = true;
