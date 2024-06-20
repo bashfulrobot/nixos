76f1f8c74e269806aa5c610c4cf354239519db88
bashfulrobot <dustin@bashfulrobot.com>
2024-06-20 14:28:30 -0700

    refactor: Refactor default.nix to improve desktop app opening
    - Update `makeDesktopApp` function to use a script for opening the application
    - Modify `exec` property of `desktopItem` to use the script path instead of the binary directly
    - Add assertions for required parameters in `makeDesktopApp` function
    
    [lib/cbb-webwrap/default.nix]
    - Modify the `makeDesktopApp` function to use a script to open the application
    - Update the `exec` property of the `desktopItem` to use the script path instead of the binary directly
    - Add assertions for the required parameters in the `makeDesktopApp` function

--- lib/cbb-webwrap/default.nix
+++ lib/cbb-webwrap/default.nix
@@ -3,45 +3,62 @@
 let
   makeDesktopApp = { name, url, binary, startupWMClass, iconSizes, iconPath }:
     let
-      desktopName = lib.strings.toLower (lib.strings.replaceStrings [" "] ["_"] name);
+      desktopName =
+        lib.strings.toLower (lib.strings.replaceStrings [ " " ] [ "_" ] name);
+      scriptPath = pkgs.writeShellScriptBin desktopName ''
+        ${binary} --ozone-platform-hint=auto --force-dark-mode --enable-features=WebUIDarkMode --new-window --app="${url}"
+      '';
       desktopItem = makeDesktopItem {
         type = "Application";
         name = desktopName;
         desktopName = name;
         startupWMClass = startupWMClass;
-        exec = ''
-          ${binary} --ozone-platform-hint=auto --force-dark-mode --enable-features=WebUIDarkMode --new-window --app="${url}" %U'';
+        # exec = ''
+        #   ${binary} --ozone-platform-hint=auto --force-dark-mode --enable-features=WebUIDarkMode --new-window --app="${lib.escapeShellArg url}"
+        # '';
+        exec = "${scriptPath}/bin/${desktopName}"; # use the script to open the app - accounts for url encoding. which breaks due to the desktop file spec.
         icon = desktopName;
         categories = [ "Application" ];
       };
 
-      icons = map (size: pkgs.stdenv.mkDerivation {
-        name = "${desktopName}-icon-${size}";
-        src = "${iconPath}/${size}.png";
-        phases = [ "installPhase" ];
-        installPhase = ''
-          mkdir -p $out/share/icons/hicolor/${size}x${size}/apps
-          cp $src $out/share/icons/hicolor/${size}x${size}/apps/${desktopName}.png
-        '';
-      }) iconSizes;
-    in
-    {
+      icons = map (size:
+        pkgs.stdenv.mkDerivation {
+          name = "${desktopName}-icon-${size}";
+          src = "${iconPath}/${size}.png";
+          phases = [ "installPhase" ];
+          installPhase = ''
+            mkdir -p $out/share/icons/hicolor/${size}x${size}/apps
+            cp $src $out/share/icons/hicolor/${size}x${size}/apps/${desktopName}.png
+          '';
+        }) iconSizes;
+    in {
       assertions = [
-        { assertion = name != null;
-          message = "name is a required parameter for makeDesktopApp"; }
-        { assertion = url != null;
-          message = "url is a required parameter for makeDesktopApp"; }
-        { assertion = binary != null;
-          message = "binary is a required parameter for makeDesktopApp"; }
-        { assertion = startupWMClass != null;
-          message = "startupWMClass is a required parameter for makeDesktopApp"; }
-        { assertion = iconSizes != null;
-          message = "iconSizes is a required parameter for makeDesktopApp"; }
-        { assertion = iconPath != null;
-          message = "iconPath is a required parameter for makeDesktopApp"; }
+        {
+          assertion = name != null;
+          message = "name is a required parameter for makeDesktopApp";
+        }
+        {
+          assertion = url != null;
+          message = "url is a required parameter for makeDesktopApp";
+        }
+        {
+          assertion = binary != null;
+          message = "binary is a required parameter for makeDesktopApp";
+        }
+        {
+          assertion = startupWMClass != null;
+          message = "startupWMClass is a required parameter for makeDesktopApp";
+        }
+        {
+          assertion = iconSizes != null;
+          message = "iconSizes is a required parameter for makeDesktopApp";
+        }
+        {
+          assertion = iconPath != null;
+          message = "iconPath is a required parameter for makeDesktopApp";
+        }
       ];
       desktopItem = desktopItem;
       icons = icons;
     };
-in
-  makeDesktopApp
+in makeDesktopApp
