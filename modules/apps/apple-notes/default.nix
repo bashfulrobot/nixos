1b16f501e6d2208bf67d018d1f88e27f872bc770
bashfulrobot <dustin@bashfulrobot.com>
2024-06-25 14:31:03 -0700

    feat: Add Apple Notes app files and icons
    - Added new files `48.png`, `64.png`, `32.png`, `96.png`, `default.nix`, `256.png`, and `128.png` to the `modules/apps/apple-notes/icons` directory
    - Created options for enabling the Apple Notes app in the `default.nix` file
    - Added system packages and icons to the environment when the app is enabled
    
    [modules/apps/apple-notes/icons/48.png]
    - Add new file `modules/apps/apple-notes/icons/48.png`
    [modules/apps/apple-notes/icons/64.png]
    - Add new file `64.png` in `modules/apps/apple-notes/icons/` directory
    [modules/apps/apple-notes/icons/32.png]
    - Added a new file `/modules/apps/apple-notes/icons/32.png`
    [modules/apps/apple-notes/icons/96.png]
    - Added a new file `96.png` to the `apple-notes/icons` folder.
    [modules/apps/apple-notes/default.nix]
    - Add a new file `modules/apps/apple-notes/default.nix`
    - Create options for enabling the Apple Notes app
    - Add system packages and icons to the environment when the app is enabled
    [modules/apps/apple-notes/icons/256.png]
    - A new file `256.png` was added to the `apple-notes/icons` directory in the `modules/apps` module.
    [modules/apps/apple-notes/icons/128.png]
    - Added a new file: `modules/apps/apple-notes/icons/128.png`

---
+++ modules/apps/apple-notes/default.nix
@@ -0,0 +1,41 @@
+# https://raw.githubusercontent.com/Gako358/dotfiles/6236f706279d2450606dfc99fecce9399936b7e7/home/programs/browser/teams.nix
+{ user-settings, config, pkgs, lib, makeDesktopItem, ... }:
+
+let
+  cfg = config.apple-notes;
+
+  # Import the makeDesktopApp function
+  makeDesktopApp = pkgs.callPackage ../../../lib/cbb-webwrap { };
+
+  # I temp create an app in brave to download all the icons, then I place then in the correct folder
+  gcalBashfulrobotApp = makeDesktopApp {
+    name = "Apple Notes";
+    url = "https://www.icloud.com/notes/";
+    binary = "${pkgs.brave}/bin/brave";
+    startupWMClass = "brave-www.icloud.com__notes_-Default";
+    iconSizes = [ "32" "48" "64" "96" "128" "256" ];
+    # iconSizes = [ "256" ]; # forcing large icon use
+    iconPath = ./icons; # path to icons
+  };
+
+in {
+
+  options = {
+    apple-notes.enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Enable the Bashfulrobot Google Calendar app.";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+
+    environment.systemPackages = [ gcalBashfulrobotApp.desktopItem ]
+      ++ gcalBashfulrobotApp.icons;
+
+    # home-manager.users."${user-settings.user.username}" = {
+
+    # };
+  };
+
+}
