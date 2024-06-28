0bd1d0053e6491bab09bb20e8cc64dec77a9b0d6
bashfulrobot <dustin@bashfulrobot.com>
2024-06-28 11:46:11 -0700

    feat: Refactor fish shell configuration and completions
    - Modify the `default.nix` file for the fish shell
    - Enable and manage fish configuration and plugins with Home Manager
    - Add vendor fish completions provided by Nixpkgs
    - Introduce functions for shell commands and operations
    - Include shell abbreviations and aliases
    
    [modules/cli/fish/default.nix]
    - Modify the `lib.modules.cli.fish.default.nix` file
    - Enable the fish shell
    - Enable the fish shell and manage fish configuration and plugins with Home Manager
    - Enable vendor fish completions provided by Nixpkgs
    - Add functions for various shell commands and operations
    - Add shell abbreviations and aliases

--- modules/cli/fish/default.nix
+++ modules/cli/fish/default.nix
@@ -290,6 +290,8 @@ in {
           upgrade =
             "clear && cd ~/dev/nix/nixos/; ${pkgs.just}/bin/just upgrade-system";
           dev-rebuild =
+            "clear && cd ~/dev/nix/nixos/; rm -f /home/dustin/.config/mimeapps.list && ${pkgs.just}/bin/just dev-test";
+          test-rebuild =
             "clear && cd ~/dev/nix/nixos/; rm -f /home/dustin/.config/mimeapps.list && ${pkgs.just}/bin/just dev-rebuild";
           kubitect =
             "${pkgs.steam-run}/bin/steam-run /etc/profiles/per-user/dustin/bin/kubitect";
