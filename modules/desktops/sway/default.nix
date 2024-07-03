50570d6b4dacf06ef81806879639f0deadbb6442
bashfulrobot <dustin@bashfulrobot.com>
2024-07-03 09:36:24 -0700

    style: Remove `--screenshots` option from `swaylock` command
    - Remove the `--screenshots` option from the `swaylock` command in `modules/desktops/sway/default.nix` file
    
    [modules/desktops/sway/default.nix]
    - Remove the `--screenshots` option from the `swaylock` command

--- modules/desktops/sway/default.nix
+++ modules/desktops/sway/default.nix
@@ -31,7 +31,7 @@ let
             timeout 10 'swaymsg "output * dpms off"' \
             resume 'swaymsg "output * dpms on"' &
       # Locks the screen immediately
-        swaylock --screenshots --clock --indicator --indicator-radius 100 --indicator-thickness 7 --effect-blur 7x5 --effect-vignette 0.5:0.5 --grace 2 --fade-in 0.2
+        swaylock --clock --indicator --indicator-radius 100 --indicator-thickness 7 --effect-blur 7x5 --effect-vignette 0.5:0.5 --grace 2 --fade-in 0.2
         # Kills last background task so idle timer doesn't keep running
         kill %%
 
