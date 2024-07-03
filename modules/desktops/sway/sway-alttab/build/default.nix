e365b5402aeb102c7c13ce18fe6469c8e462a005
bashfulrobot <dustin@bashfulrobot.com>
2024-07-02 18:11:48 -0700

    chore: Update sway-alttab to latest version and simplify derivation
    - Updated the download URL and sha256 hash for sway-alttab to the latest version
    - Simplified the derivation for sway-alttab by removing unnecessary phases
    
    [modules/desktops/sway/sway-alttab/build/default.nix]
    - Update the download URL for sway-alttab to the latest version
    - Update the sha256 hash for sway-alttab download
    - Simplify the derivation by removing all phases except the installPhase

--- modules/desktops/sway/sway-alttab/build/default.nix
+++ modules/desktops/sway/sway-alttab/build/default.nix
@@ -9,8 +9,8 @@ pkgs.stdenvNoCC.mkDerivation {
 
   src = pkgs.fetchurl {
     url =
-      "https://github.com/autolyticus/sway-alttab/releases/tag/v1.1.3";
-    sha256 = "sha256-vnz5N59tTvgU/G1BEabsyFCpA7OdTxpLXrzFN4jhzeg=";
+      "https://github.com/autolyticus/sway-alttab/releases/download/v1.1.3/sway-alttab";
+    sha256 = "sha256-EiehesGZpUHAY+uwiiJj1+vrdQ4YetYzFTpbE89cwO0=";
   };
 
   phases = [ "installPhase" ]; # Removes all phases except installPhase
