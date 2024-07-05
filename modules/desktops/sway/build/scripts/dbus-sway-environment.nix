5956209ad6ddff1be6202efa9a17dd194207c5de
bashfulrobot <dustin@bashfulrobot.com>
2024-07-04 18:23:12 -0700

    chore: Improve scripts and file formatting in sway build.
    - Add comment explaining purpose of dbus-sway-environment.nix script
    - Remove newline at end of dbus-sway-environment.nix file
    
    [modules/desktops/sway/build/scripts/dbus-sway-environment.nix]
    - Add a comment explaining the purpose of the script
    - Remove the newline at the end of the file

--- modules/desktops/sway/build/scripts/dbus-sway-environment.nix
+++ modules/desktops/sway/build/scripts/dbus-sway-environment.nix
@@ -7,7 +7,7 @@ pkgs.writeShellApplication {
 
   text = ''
     #!/usr/bin/env bash
-
+    # https://github.com/swaywm/sway/wiki#gtk-applications-take-20-seconds-to-start
     dbus-update-activation-environment --systemd DISPLAY SWAYSOCK WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=sway
     systemctl --user stop pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
     systemctl --user start pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
