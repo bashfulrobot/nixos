540156562ea024477917bbca465b206575de8f17
bashfulrobot <dustin@bashfulrobot.com>
2024-07-11 15:33:29 -0700

    feat: yolo

---
+++ shll.nix
@@ -0,0 +1,13 @@
+# Shell for bootstrapping flake-enabled nix and home-manager
+# Access development shell with  'nix develop' or (legacy) 'nix-shell'
+{ pkgs ? (import ./nixpkgs.nix) { } }: {
+  default = pkgs.mkShell {
+    name = "bashfulrobot-bootstrap--flake";
+    # Enable experimental features without having to specify the argument
+    NIX_CONFIG = "experimental-features = nix-command flakes";
+    nativeBuildInputs = with pkgs; [ nix home-manager git ];
+    shellHook = ''
+      exec zsh
+    '';
+  };
+}
