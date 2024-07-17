1db8c73b56938b6b800e746a85fd3bc8d3c229f9
bashfulrobot <dustin@bashfulrobot.com>
2024-07-17 10:10:21 -0700

    chore: sound option removed
    - upstread removed to option due to it being overloaded and no longer
    having an effect.

--- modules/hw/audio/default.nix
+++ modules/hw/audio/default.nix
@@ -12,7 +12,9 @@ in {
 
   config = lib.mkIf cfg.enable {
     # Enable sound with pipewire.
-    sound.enable = true;
+    # The option definition `sound' no longer has any effect; please remove it.
+    #The option was heavily overloaded and can be removed from most configurations.
+    #sound.enable = true;
     hardware.pulseaudio.enable = false;
     security.rtkit.enable = true;
     services.pipewire = {
