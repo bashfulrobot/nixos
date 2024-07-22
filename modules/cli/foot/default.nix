0f8f378adebcafed5bb701f293cc2a4706fdaa70
bashfulrobot <dustin@bashfulrobot.com>
2024-07-22 08:35:55 -0700

    feat: update foot settings

--- modules/cli/foot/default.nix
+++ modules/cli/foot/default.nix
@@ -20,12 +20,29 @@ in {
         settings = {
           main = {
             shell = "${pkgs.fish}/bin/fish";
-            font = "Fira Code:size=12";
+            # font = "Fira Code:size=12";
+            # font = "Code OnePiece:size=26, Noto Color Emoji:size=25";
+            # font-bold = "Code OnePiece:size=26, Noto Color Emoji:size=25";
+            letter-spacing = "1";
+            box-drawings-uses-font-glyphs = "no";
             pad = "12x12";
+            # pad = "0x0center";
             dpi-aware = "yes";
             selection-target = "both";
           };
+
+          scrollback = {
+            lines = 10000;
+            multiplier = 3;
+          };
+
           mouse = { hide-when-typing = "yes"; };
+
+          key-bindings = {
+            clipboard-copy = "Control+Shift+c";
+            clipboard-paste = "Control+Shift+v Control+y";
+            primary-paste = "Shift+Insert";
+          };
 
         };
       };
