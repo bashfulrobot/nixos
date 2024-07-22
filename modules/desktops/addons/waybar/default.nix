b94b1d0a10174dcde2764a80585719f8495648d5
bashfulrobot <dustin@bashfulrobot.com>
2024-07-22 08:36:24 -0700

    chore: adding hyprland items to waybar.
    - foundational work

--- modules/desktops/addons/waybar/default.nix
+++ modules/desktops/addons/waybar/default.nix
@@ -1,11 +1,6 @@
 # Icon search - https://fontawesome.com/search
-{
-  user-settings,
-  pkgs,
-  config,
-  lib,
-  ...
-}: let
+{ user-settings, pkgs, config, lib, ... }:
+let
   cfg = config.desktops.addons.waybar;
   # Used in my home manager code at the bottom of the file.
 in {
@@ -18,7 +13,7 @@ in {
   };
 
   config = lib.mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [font-awesome work-sans];
+    environment.systemPackages = with pkgs; [ font-awesome work-sans ];
     home-manager.users."${user-settings.user.username}" = {
       programs.waybar = {
         enable = true;
@@ -271,11 +266,37 @@ in {
             "height": 5, // Waybar height (to be removed for auto height)
             // "width": 1280, // Waybar width
             // Choose the order of the modules
-            "modules-left": ["sway/workspaces", "sway/mode", "custom/media"],
-            "modules-center": ["sway/window"],
+            "modules-left": ["hyprland/workspaces", "sway/workspaces", "sway/mode", "custom/media"],
+            "modules-center": ["hyprland/window, "sway/window"],
             //"modules-right": ["pulseaudio", "network", "backlight","cpu","memory","battery", "battery#bat2", "clock", "tray"],
             "modules-right": ["pulseaudio", "network", "backlight", "battery","clock", "tray"],
             // Modules configuration
+            "hyprland/submap": {
+          "format": " {}"
+          "max-length": 14
+          "tooltip": false
+        }
+            "hyprland/workspaces": {
+                 "disable-scroll": true,
+                 "all-outputs": true,
+                 "show-special": true,
+                 "format": "{name}: {icon}",
+                 "format-icons": {
+                     "1": "",
+                     "2": "",
+                     "3": "",
+                     "4": "",
+                     "5": "",
+                     "6": "",
+                     "7": "",
+                     "8": "",
+                     "9": "",
+                     "10": "",
+                     "urgent": "",
+                     "focused": "",
+                     // "default": ""
+                 }
+             },
             "sway/workspaces": {
                  "disable-scroll": true,
                  "all-outputs": true,
@@ -296,6 +317,18 @@ in {
                      // "default": ""
                  }
              },
+             "hyprland/window": {
+          "format": "{}"
+          "separate-outputs": true
+          "max-length": 35
+          "rewrite": {
+            "(.*) - Mozilla Firefox": "🦊 $1"
+            "(.*) - LibreWolf": "🐺 $1"
+            "(.*) - Brave":  "🦁 $1"
+            "(.*).epub(.*)": "󰂽 $1"
+            "(.*)foot": " Terminal $1"
+          }
+        },
             "sway/mode": {
                 "format": "<span style=\"italic\">{}</span>"
             },
