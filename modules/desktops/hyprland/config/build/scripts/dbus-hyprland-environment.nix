5211f0822e38772d17734e2be355763330135eb9
bashfulrobot <dustin@bashfulrobot.com>
2024-07-25 09:59:39 -0700

    bug: missing dbus sesson variables for screensharing

--- modules/desktops/hyprland/config/build/scripts/dbus-hyprland-environment.nix
+++ modules/desktops/hyprland/config/build/scripts/dbus-hyprland-environment.nix
@@ -8,8 +8,9 @@ pkgs.writeShellApplication {
   text = ''
     #!/run/current-system/sw/bin/env bash
 
-    dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
+    dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY XDG_CURRENT_DESKTOP SWAYSOCK QT_QPA_PLATFORMTHEME
     dbus-update-activation-environment --systemd --all
+    systemctl --user import-environment QT_QPA_PLATFORMTHEME
 
     systemctl --user stop pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-hyprland xdg-desktop-portal-gtk
     systemctl --user start pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-hyprland xdg-desktop-portal-gtk
