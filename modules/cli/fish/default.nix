4d08ea6d4187070fad97a14473c453468fec0bc6
bashfulrobot <dustin@bashfulrobot.com>
2024-07-29 11:59:30 -0700

    feat: create copy icons script and alias
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   modules/cli/fish/default.nix
    	new file:   modules/sys/scripts/copy-icons/default.nix

--- modules/cli/fish/default.nix
+++ modules/cli/fish/default.nix
@@ -268,7 +268,8 @@ in {
           gc = "git add . && git commit -S && git push && git pull";
         };
         shellAliases = {
-          # show-sway-bindings = "bat ~/.config/sway/config | rg bindsym";
+          copy-icons = "copy_icons";
+
           hm-logs =
             "sudo systemctl restart home-manager-dustin.service; journalctl -xeu home-manager-dustin.service";
           tailscale-up-lt =
---
+++ modules/sys/scripts/copy-icons/default.nix
@@ -0,0 +1,53 @@
+{ user-settings, pkgs, lib, config, ... }:
+
+let
+  cfg = config.sys.scripts.restic;
+  copyIcons = pkgs.writeShellApplication {
+    name = "copy_icons";
+
+    runtimeInputs = [ ];
+
+    text = ''
+      #!/usr/bin/env bash
+
+      # Copy all icons from Chromium Web Apps to the target directory
+
+      # I create the web app in brave temporarily to get all the icons at multiple sizes
+      # then I copy them to the target directory
+      # then remove the brave web app and define it in nix
+
+      if [ -z "$1" ]; then
+          echo "Error: No target directory provided."
+          echo "Usage: $0 target_directory"
+          exit 1
+      fi
+
+      target_dir=$1
+      target_path="$HOME/dev/nix/nixos/modules/apps/$target_dir/icons/"
+
+      if [ ! -d "$target_path" ]; then
+          mkdir -p $target_path
+      fi
+
+      # Copy all files from the Icons subdirectory to the target directory
+      cp $HOME/.config/chromium/Default/Web\ Applications/Manifest\ Resources/*/Icons/* $target_path
+
+      exit 0
+    '';
+  };
+in {
+  options = {
+    sys.scripts.restic.enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Enable the copy_icons.";
+    };
+  };
+  config = lib.mkIf cfg.enable {
+    #   environment.systemPackages = [ copyIcons ];
+    home-manager.users."${user-settings.user.username}" = {
+      home.packages = with pkgs; [ copyIcons ];
+    };
+  };
+
+}
