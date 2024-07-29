67a132dfa77c49854f8f6fe8fc7e6dbf4b0a8756
bashfulrobot <dustin@bashfulrobot.com>
2024-07-29 11:54:51 -0700

    add: intercom web app
    On branch main
    Your branch is ahead of 'origin/main' by 4 commits.
      (use "git push" to publish your local commits)
    
    Changes to be committed:
    	new file:   modules/apps/intercom/default.nix
    	new file:   modules/apps/intercom/icons/128.png
    	new file:   modules/apps/intercom/icons/256.png
    	new file:   modules/apps/intercom/icons/32.png
    	new file:   modules/apps/intercom/icons/48.png
    	new file:   modules/apps/intercom/icons/64.png
    	new file:   modules/apps/intercom/icons/96.png

---
+++ modules/apps/intercom/default.nix
@@ -0,0 +1,41 @@
+# https://raw.githubusercontent.com/Gako358/dotfiles/6236f706279d2450606dfc99fecce9399936b7e7/home/programs/browser/teams.nix
+{ user-settings, config, pkgs, lib, makeDesktopItem, ... }:
+
+let
+  cfg = config.apps.intercom;
+
+  # Import the makeDesktopApp function
+  makeDesktopApp = pkgs.callPackage ../../../lib/cbb-webwrap { };
+
+  # I temp create an app in brave to download all the icons, then I place then in the correct folder
+  intercomApp = makeDesktopApp {
+    name = "Intercom";
+    url = "https://app.intercom.com/a/inbox/tdx7wtfd/inbox/team/3988200";
+    binary = "${pkgs.chromium}/bin/chromium";
+    myStartupWMClass = "chrome-app.intercom.com__a_inbox_tdx7wtfd_inbox_team_3988200-Default";
+    iconSizes = [ "32" "48" "64" "96" "128" "256"];
+    # iconSizes = [ "256" ]; # forcing large icon use
+    iconPath = ./icons; # path to icons
+  };
+
+in {
+
+  options = {
+    apps.intercom.enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Enable the Intercom app.";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+
+    environment.systemPackages = [ intercomApp.desktopItem ]
+      ++ intercomApp.icons;
+
+    # home-manager.users."${user-settings.user.username}" = {
+
+    # };
+  };
+
+}
