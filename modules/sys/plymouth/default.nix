7963b4c47fa575ba28caebdc83c0108abf018f63
bashfulrobot <dustin@bashfulrobot.com>
2024-07-30 16:20:07 -0700

    feat: device scaling in plymouth

--- modules/sys/plymouth/default.nix
+++ modules/sys/plymouth/default.nix
@@ -25,6 +25,10 @@ in {
         url = "https://nixos.org/logo/nixos-hires.png";
         sha256 = "1ivzgd7iz0i06y36p8m5w48fd8pjqwxhdaavc0pxs7w1g7mcy5si";
       };
+      extraConfig = ''
+        ShowDelay=2
+        DeviceScale=1
+      '';
     };
 
   };
