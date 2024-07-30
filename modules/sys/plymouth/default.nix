74bf2cdd83ce82046483ce13dc7e4637c519e213
bashfulrobot <dustin@bashfulrobot.com>
2024-07-30 16:15:40 -0700

    feat: add plymouth module

---
+++ modules/sys/plymouth/default.nix
@@ -0,0 +1,31 @@
+{ config, pkgs, lib, ... }:
+let cfg = config.sys.plymouth;
+in {
+
+  options = {
+    sys.plymouth.enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Enable Plymouth.";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      dconf
+      dconf2nix
+      dconf-editor
+
+    ];
+    # Enable dconf
+    programs.plymouth = {
+      enable = true;
+      font = "${pkgs.work-sans}/share/fonts/opentype/WorkSans-Regular.ttf";
+      logo = pkgs.fetchurl {
+        url = "https://nixos.org/logo/nixos-hires.png";
+        sha256 = "1ivzgd7iz0i06y36p8m5w48fd8pjqwxhdaavc0pxs7w1g7mcy5si";
+      };
+    };
+
+  };
+}
