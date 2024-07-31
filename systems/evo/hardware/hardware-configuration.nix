879db7d9947d2f32f67fcd5393c97afda97f7abd
bashfulrobot <dustin@bashfulrobot.com>
2024-07-30 21:06:04 -0700

    chore: add new files for evo reload
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   systems/evo/hardware/boot.nix
    	modified:   systems/evo/hardware/hardware-configuration.nix

--- systems/evo/hardware/hardware-configuration.nix
+++ systems/evo/hardware/hardware-configuration.nix
@@ -10,26 +10,21 @@
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
   boot.initrd.kernelModules = [ ];
-  # May have been a helper in getting my nvidia gpu going
-  boot.initrd.systemd.enable = true;
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/a4137ef8-b74e-4098-a446-d30c2aa0baeb";
+    { device = "/dev/disk/by-uuid/9b5ab339-3ce4-475c-b1a5-854173abcd75";
       fsType = "ext4";
     };
 
-  boot.initrd.luks.devices."luks-54580df8-e0e8-442e-af0f-5c0ac1a37a44".device = "/dev/disk/by-uuid/54580df8-e0e8-442e-af0f-5c0ac1a37a44";
-
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/AC2D-7457";
+    { device = "/dev/disk/by-uuid/6018-7528";
       fsType = "vfat";
+      options = [ "fmask=0022" "dmask=0022" ];
     };
 
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/1aba9cbc-afd0-4598-828a-4a173076747c"; }
-    ];
+  swapDevices = [ ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
