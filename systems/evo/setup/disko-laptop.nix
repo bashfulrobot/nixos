9d5adcfb4266c7821185efedbf14c20091cf1979
bashfulrobot <dustin@bashfulrobot.com>
2024-07-30 17:47:10 -0700

    feat: add disko config for laptop

---
+++ systems/evo/setup/disko-laptop.nix
@@ -0,0 +1,32 @@
+{
+  disko.devices = {
+    disk = {
+      main = {
+        device = "/dev/nvme0n1";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              type = "EF00";
+              size = "3000M";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
