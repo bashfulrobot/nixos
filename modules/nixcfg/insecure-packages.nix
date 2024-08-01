845867f8cb5d2245632ee52f3f777e38e0f25df5
bashfulrobot <dustin@bashfulrobot.com>
2024-08-01 08:23:19 -0700

    test: remove insecure-packages
    - want to see if this particular package is still
    needed
    
    On branch main
    Your branch is ahead of 'origin/main' by 1 commit.
      (use "git push" to publish your local commits)
    
    Changes to be committed:
    	modified:   modules/nixcfg/insecure-packages.nix

--- modules/nixcfg/insecure-packages.nix
+++ modules/nixcfg/insecure-packages.nix
@@ -14,7 +14,7 @@ in {
 
     nixpkgs.config.permittedInsecurePackages =
       # [ "electron-22.3.27" "electron-24.8.6" "openssl-1.1.1v" "openssl-1.1.1w" ];
-      [ "electron-25.9.0" ];
+      # [ "electron-25.9.0" ];
   };
 
 }
