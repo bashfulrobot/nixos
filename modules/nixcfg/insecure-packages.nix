8b67444f607703f1f75716545b4af3d108db5904
bashfulrobot <dustin@bashfulrobot.com>
2024-08-01 08:32:30 -0700

    bug: missed commenting out a portion

--- modules/nixcfg/insecure-packages.nix
+++ modules/nixcfg/insecure-packages.nix
@@ -12,7 +12,7 @@ in {
 
   config = lib.mkIf cfg.enable {
 
-    nixpkgs.config.permittedInsecurePackages =
+    # nixpkgs.config.permittedInsecurePackages =
       # [ "electron-22.3.27" "electron-24.8.6" "openssl-1.1.1v" "openssl-1.1.1w" ];
       # [ "electron-25.9.0" ];
   };
