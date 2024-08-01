64aba3aae07242cb2fafcf3f6cc64c0f7bc541bb
bashfulrobot <dustin@bashfulrobot.com>
2024-08-01 08:15:39 -0700

    chore: Enable all firmware regarldess of license
    - hardware.enableAllFirmware = true;
    
    Changes to be committed:
    	modified:   modules/hw/fwupd/default.nix
    
    Changes not staged for commit:
    	modified:   modules/apps/chrome-based-browser/default.nix

--- modules/hw/fwupd/default.nix
+++ modules/hw/fwupd/default.nix
@@ -13,5 +13,8 @@ in {
   config = lib.mkIf cfg.enable {
     # Enable Firmware Updates
     services.fwupd.enable = true;
+
+    # Enable all firmware regarldess of license
+    hardware.enableAllFirmware = true;
   };
 }
