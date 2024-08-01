3494137dd8d04a3c2895becc34b1ff2113d8c9d8
bashfulrobot <dustin@bashfulrobot.com>
2024-08-01 06:02:52 -0700

    chore: clean up boot on rembot

--- systems/rembot/hardware/boot.nix
+++ systems/rembot/hardware/boot.nix
@@ -1,23 +1,13 @@
 { config, pkgs, ... }: {
 
   # Bootloader.
-  boot = {
-    loader = {
-      systemd-boot = {
-        enable = true;
-        consoleMode = "max"; # ensure windows will be found in boot menu
-        netbootxyz.enable = false;
-      };
-      efi.canTouchEfiVariables = true;
-    };
-    kernel = {
-      sysctl = {
-        # Enable traffic between interfaces
-        # "net.ipv4.conf.all.forwarding" = true;
-        # "net.ipv6.conf.all.forwarding" = true;
-        # Now defined in KVM routing
-      };
-    };
+  boot.loader.systemd-boot = {
+    enable = true;
+    consoleMode = "max"; # ensure windows will be found in boot menu
+    netbootxyz.enable = false;
   };
 
+
+  boot.loader.efi.canTouchEfiVariables = true;
+  
 }
