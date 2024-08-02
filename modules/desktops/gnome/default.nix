e8866883acdbb5d72d53cba8c795a2911cb6e331
bashfulrobot <dustin@bashfulrobot.com>
2024-08-02 10:45:02 -0700

    bug: disable triple buffering gnome patch.
    - breaks my build as stylix uses aliases.

--- modules/desktops/gnome/default.nix
+++ modules/desktops/gnome/default.nix
@@ -116,25 +116,28 @@ in {
 
 # MUTTER PATCH--------------------------------------------------------------
 # NOTE - this could be something to watch as gnome updates
+#
+# Currently breaks my build with Stylix.
+#
 # More info (Dynamic triple buffering) - https://wiki.nixos.org/wiki/GNOME
-    nixpkgs.overlays = [
-      # GNOME 46: triple-buffering-v4-46
-      (final: prev: {
-        gnome = prev.gnome.overrideScope (gnomeFinal: gnomePrev: {
-          mutter = gnomePrev.mutter.overrideAttrs (old: {
-            src = pkgs.fetchFromGitLab {
-              domain = "gitlab.gnome.org";
-              owner = "vanvugt";
-              repo = "mutter";
-              rev = "triple-buffering-v4-46";
-              hash = "sha256-fkPjB/5DPBX06t7yj0Rb3UEuu5b9mu3aS+jhH18+lpI=";
-            };
-          });
-        });
-      })
-    ];
+    # nixpkgs.overlays = [
+    #   # GNOME 46: triple-buffering-v4-46
+    #   (final: prev: {
+    #     gnome = prev.gnome.overrideScope (gnomeFinal: gnomePrev: {
+    #       mutter = gnomePrev.mutter.overrideAttrs (old: {
+    #         src = pkgs.fetchFromGitLab {
+    #           domain = "gitlab.gnome.org";
+    #           owner = "vanvugt";
+    #           repo = "mutter";
+    #           rev = "triple-buffering-v4-46";
+    #           hash = "sha256-fkPjB/5DPBX06t7yj0Rb3UEuu5b9mu3aS+jhH18+lpI=";
+    #         };
+    #       });
+    #     });
+    #   })
+    # ];
 
-    nixpkgs.config.allowAliases = false;
+    # nixpkgs.config.allowAliases = false;
 # MUTTER PATCH--------------------------------------------------------------
 
     ##### Home Manager Config options #####
