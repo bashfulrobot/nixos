d43632bf9c321dc5fa012d43222546e240ddde95
bashfulrobot <dustin@bashfulrobot.com>
2024-08-02 12:08:40 -0700

    feat: add stylix support to dark-reader

--- modules/sys/stylix/default.nix
+++ modules/sys/stylix/default.nix
@@ -1,8 +1,9 @@
 # # Keychain set-up
 
 { user-settings, config, lib, pkgs, ... }:
-let cfg = config.sys.stylix;
-sf-mono-liga-font = pkgs.callPackage ../fonts/build/sfpro/liga { };
+let
+  cfg = config.sys.stylix;
+  sf-mono-liga-font = pkgs.callPackage ../fonts/build/sfpro/liga { };
 
 in {
 
@@ -23,7 +24,7 @@ in {
       imageScalingMode = "fill";
       polarity = "dark"; # or "light" - forcing dark
       # base16Scheme =
-        # "${pkgs.base16-schemes}/share/themes/tokyo-night-dark.yaml";
+      # "${pkgs.base16-schemes}/share/themes/tokyo-night-dark.yaml";
       cursor = {
         package = pkgs.bibata-cursors;
         name = "Bibata-Modern-Ice";
@@ -63,6 +64,80 @@ in {
         };
       };
 
+    };
+
+    # Add a hacky way to use stylix with dark-reader browser extension
+    xdg = {
+      configFile = {
+
+        # Creates a config for DarkReader
+        # TODO, need to make this auto update?
+        darkreader = {
+          enable = true;
+          #onChange = manually tell darkreader to refresh somehow?
+          target = "darkreader/config.json";
+          text = ''
+
+            	{
+            	    "schemeVersion": 2,
+            	    "enabled": true,
+            	    "fetchNews": true,
+            	    "theme": {
+                    "mode": 1,
+                    "brightness": 100,
+                    "contrast": 100,
+                    "grayscale": 0,
+                    "sepia": 0,
+                    "useFont": false,
+                    "fontFamily": "Open Sans",
+                    "textStroke": 0,
+                    "engine": "dynamicTheme",
+                    "stylesheet": "",
+                    "darkSchemeBackgroundColor": "${base00}",
+                    "darkSchemeTextColor": "${base05}",
+                    "lightSchemeBackgroundColor": "${base05}",
+                    "lightSchemeTextColor": "${base00}",
+                    "scrollbarColor": "auto",
+                    "selectionColor": "auto",
+                    "styleSystemControls": false,
+                    "lightColorScheme": "Default",
+                    "darkColorScheme": "Default",
+                    "immediateModify": false
+            	    },
+            	    "presets": [],
+            	    "customThemes": [],
+            	    "enabledByDefault": true,
+            	    "enabledFor": [],
+            	    "disabledFor": [],
+            	    "changeBrowserTheme": false,
+            	    "syncSettings": false,
+            	    "syncSitesFixes": true,
+            	    "automation": {
+                    "enabled": false,
+                    "mode": "",
+                    "behavior": "OnOff"
+            	    },
+            	    "time": {
+                    "activation": "18:00",
+                    "deactivation": "9:00"
+            	    },
+            	    "location": {
+                    "latitude": null,
+                    "longitude": null
+            	    },
+            	    "previewNewDesign": true,
+            	    "enableForPDF": true,
+            	    "enableForProtectedPages": true,
+            	    "enableContextMenus": false,
+            	    "detectDarkTheme": false,
+            	    "displayedNews": [
+                    "thanks-2023"
+            	    ]
+            	}
+
+            	'';
+        };
+      };
     };
     # home-manager.users."${user-settings.user.username}" = {
     # };
