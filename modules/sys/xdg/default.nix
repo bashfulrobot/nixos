1c6f9784d0d8c95436745b47150cb23f0ca43eef
bashfulrobot <dustin@bashfulrobot.com>
2024-08-04 22:17:23 -0700

    add cdg

--- modules/sys/xdg/default.nix
+++ modules/sys/xdg/default.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, lib, ... }:
+{ user-settings, pkgs, config, lib, ... }:
 let cfg = config.sys.xdg;
 in {
 
@@ -11,15 +11,49 @@ in {
   };
 
   config = lib.mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [
+    environment.systemPackages = with pkgs;
+      [
 
-    ];
+      ];
     # Enable xdg
     xdg = {
-      mime = {
-        enable = true;
-      };
+      mime.enable = true;
+      autostart.enable = true;
+      icons.enable = true;
+      menus.enable = true;
+      sounds.enable = true;
     };
 
+    home-manager.users."${user-settings.user.username}" = {
+      xdg = {
+        # desktopEntries.librewolf = {
+        #   name = "LibreWolf";
+        #   exec = "${pkgs.librewolf}/bin/librewolf";
+        # };
+        mimeApps = {
+          associations = {
+            # added = { "x-scheme-handler/tg" = "org.telegram.desktop.desktop"; };
+          };
+          enable = true;
+          defaultApplications = {
+            "text/html" = [ "chromium.desktop" ];
+            "text/plain" = [ "code.desktop" ];
+            # "inode/directory" = [ "lf.desktop" ];
+            # "application/pdf" = [ "okular.desktop" ];
+            "applications/x-www-browser" = [ "chromium.desktop" ];
+            "x-scheme-handler/about" = [ "chromium.desktop" ];
+            "x-scheme-handler/http" = [ "chromium.desktop" ];
+            "x-scheme-handler/https" = [ "chromium.desktop" ];
+            "x-scheme-handler/mailto" = [ "chromium.desktop" ];
+            "x-scheme-handler/msteams" = [ "teams-for-linux.desktop" ];
+            "x-scheme-handler/postman" = [ "Postman.desktop" ];
+            "x-scheme-handler/unknown" = [ "chromium.desktop" ];
+            "x-scheme-handler/slack" = [ "slack.desktop" ];
+            # "x-scheme-handler/tg" = [ "org.telegram.desktop.desktop" ];
+            "x-scheme-handler/webcal" = [ "chromium.desktop" ];
+          };
+        };
+      };
+    };
   };
 }
