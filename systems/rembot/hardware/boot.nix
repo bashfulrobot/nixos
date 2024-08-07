6126f4087f1e44916710eaee8d61051dd85391c9
bashfulrobot <dustin@bashfulrobot.com>
2024-08-07 12:36:48 -0700

    chore: Set max generations in boot menu to 5
    - configurationLimit = 5
    - Maximum number of latest generations in the boot
    menu. Useful to prevent boot partition running out
    of disk space.
    
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   systems/evo/hardware/boot.nix
    	modified:   systems/rembot/hardware/boot.nix

--- systems/rembot/hardware/boot.nix
+++ systems/rembot/hardware/boot.nix
@@ -5,9 +5,8 @@
     enable = true;
     consoleMode = "max"; # ensure windows will be found in boot menu
     netbootxyz.enable = false;
+    # Maximum number of latest generations in the boot menu. Useful to prevent boot partition running out of disk space.
+    configurationLimit = 5;
   };
 
-
   boot.loader.efi.canTouchEfiVariables = true;
-  
-}
