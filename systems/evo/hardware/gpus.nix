01e246d3d62d8168e582267c38d1ebfc283d1382
bashfulrobot <dustin@bashfulrobot.com>
2024-08-08 09:53:26 -0700

    test: removing "nvidia_uvm" as a test
    - as per: https://discourse.nixos.org/t/nvidia-drivers-not-loading/40913/11?u=brnix

--- systems/evo/hardware/gpus.nix
+++ systems/evo/hardware/gpus.nix
@@ -11,7 +11,11 @@
   boot = {
     # TODO: confirm this works
     # https://forums.developer.nvidia.com/t/550-54-14-cannot-create-sg-table-for-nvkmskapimemory-spammed-when-launching-chrome-on-wayland/284775/26
-    initrd.kernelModules = [ "nvidia" "i915" "nvidia_modeset" "nvidia_uvm" "nvidia_drm" ];
+    # initrd.kernelModules = [ "nvidia" "i915" "nvidia_modeset" "nvidia_uvm" "nvidia_drm" ];
+    # https://discourse.nixos.org/t/nvidia-drivers-not-loading/40913/11?u=brnix
+    #  removing "nvidia_uvm" as a test.
+    initrd.kernelModules = [ "nvidia" "i915" "nvidia_modeset" "nvidia_drm" ];
+
     # extraModulePackages = [ config.boot.kernelPackages.nvidia_x11 ];
     kernelParams = [ "nvidia-drm.fbdev=1" ];
   };
