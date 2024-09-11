49c352c6526bf037fac52e615d66438f5ef3186a
bashfulrobot <dustin@bashfulrobot.com>
2024-09-10 20:50:43 -0700

    bug: fix shebang in scripts by enabling envfs
    On branch main
    Your branch is ahead of 'origin/main' by 1 commit.
      (use "git push" to publish your local commits)
    
    Changes to be committed:
    	modified:   suites/terminal/default.nix

--- suites/terminal/default.nix
+++ suites/terminal/default.nix
@@ -19,6 +19,9 @@ in {
       yazi.enable = true;
     };
 
+    # A fuse filesystem that dynamically populates contents of /bin and /usr/bin/ so that it contains all executables from the PATH of the requesting process. This allows executing FHS based programs on a non-FHS system. For example, this is useful to execute shebangs on NixOS that assume hard coded locations like /bin or /usr/bin etc.
+    services.envfs.enable = true;
+
     environment.systemPackages = with pkgs; [
       # --- Shell experience
       fzf # command-line fuzzy finder
