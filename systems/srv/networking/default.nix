e5ee26268d97a92c99f13cd099c0313083f9f11e
bashfulrobot <dustin@bashfulrobot.com>
2024-09-19 15:44:05 -0700

    bug: incorrect interface name

--- systems/srv/networking/default.nix
+++ systems/srv/networking/default.nix
@@ -7,7 +7,7 @@
       192.168.168.1 srv
     '';
     defaultGateway = "192.168.169.1";
-    interfaces.enp1s0.ipv4 = {
+    interfaces.enp3s0.ipv4 = {
       addresses = [{
         address = "192.168.168.1";
         prefixLength = 23;
