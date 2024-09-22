a4a281cd8379ecb190c5527b42d73ba3b8e74f4a
bashfulrobot <dustin@bashfulrobot.com>
2024-09-21 20:51:31 -0700

    chore: revamp networking module for srv
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   systems/srv/networking/default.nix

--- systems/srv/networking/default.nix
+++ systems/srv/networking/default.nix
@@ -1,21 +1,32 @@
 { config, pkgs, ... }: {
   # Network CFG
   networking = {
+
     hostName = "srv";
-    # nameservers = [ "1.1.1.1" "9.9.9.9" ];
-    extraHosts = ''
-      192.168.168.1 srv
-    '';
-    defaultGateway = "192.168.169.1";
-    interfaces.enp3s0.ipv4 = {
-      addresses = [{
+
+    nameservers = [ "1.1.1.1" "9.9.9.9" ];
+
+    hosts =  {
+      "192.168.168.1" = ["srv" "srv.goat-cloud.ts.net"];
+      "127.0.0.1" = ["localhost"];
+    };
+
+    interfaces.enp3s0 = {
+      useDHCP = false;
+      ipv4.addresses = [{
         address = "192.168.168.1";
         prefixLength = 23;
       }];
     };
+
+    defaultGateway = {
+      address = "192.168.169.1";
+      interface = "enp3s0";
+    };
+
   };
 
   # Enable networking
-  networking.networkmanager.enable = true;
+  # networking.networkmanager.enable = true;
 
 }
