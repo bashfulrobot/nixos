60799086a3666759d659d54992850ac2db31e471
bashfulrobot <dustin@bashfulrobot.com>
2024-10-02 10:07:10 -0700

    bug: nm-wait failes on rebuild
    - This is related to udev not initializing
    devices. NetworkManager never completes startup
    because a WireGuard (tailscale) interface is
    never initialized by udev. A workaround is just
    putting the affected device into the
    unmanaged list.
    - I had seen other code samples where docker0
    was also added to the unmanaged list.
    
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   modules/hw/networking/default.nix

--- modules/hw/networking/default.nix
+++ modules/hw/networking/default.nix
@@ -21,7 +21,10 @@ in {
   config = lib.mkMerge [
     (lib.mkIf cfg.networkmanager.enable {
       # Enable networking
-      networking.networkmanager.enable = true;
+      networking.networkmanager = { enable = true;
+      # This is related to udev not initializing devices. NetworkManager never completes startup because a WireGuard (tailscale) interface is never initialized by udev. A workaround is just putting the affected device into the unmanaged list.
+      unmanaged = ["tailscale0" "docker0"];
+      };
     })
     (lib.mkIf (cfg.networkmanager.enable && cfg.wifi.powersave) {
       # Apply wifi powersaving configuration only if both networkmanager is enabled and wifi powersave is true
