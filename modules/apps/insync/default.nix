d5f168fca329d7a589128d44d80e44d4db82d89a
bashfulrobot <dustin@bashfulrobot.com>
2024-10-04 10:27:52 -0700

    feat: add insync system service
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   modules/apps/insync/default.nix

--- modules/apps/insync/default.nix
+++ modules/apps/insync/default.nix
@@ -14,7 +14,30 @@ in {
   config = lib.mkIf cfg.enable {
 
     # use regular package
-    environment.systemPackages = with pkgs; [ insync insync-nautilus insync-emblem-icons ];
+    environment.systemPackages = with pkgs; [
+      insync
+      insync-nautilus
+      insync-emblem-icons
+    ];
+
+    systemd.user.services.insync = {
+      Unit = {
+        Description =
+          "Insync - Google Drive, OneDrive, and Dropbox Syncing on Linux, Windows & Mac";
+        After = [ "graphical-session-pre.target" ];
+        PartOf = [ "graphical-session.target" ];
+      };
+
+      Install = { WantedBy = [ "graphical-session.target" ]; };
+
+      Service = {
+        Type = "simple";
+        ExecStart = "${pkgs.insync}/bin/insync start --no-daemon";
+        ExecStop = "${pkgs.insync}/bin/insync stop";
+        Restart = "always";
+        RestartSec = "5";
+      };
+    };
 
     home-manager.users."${user-settings.user.username}" = {
 
