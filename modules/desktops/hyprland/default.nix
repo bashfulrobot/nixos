a75541cb440991cf77b0e8e865a541ce6a68eb3b
bashfulrobot <dustin@bashfulrobot.com>
2024-10-09 08:47:50 -0700

    bug: col.shadow in hyprland config conflict with stylix
    - removed from hyprland
    - stylix is my theming mechanism
    
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
      modified:   modules/desktops/hyprland/default.nix

--- modules/desktops/hyprland/default.nix
+++ modules/desktops/hyprland/default.nix
@@ -223,7 +223,6 @@ in {
             drop_shadow = true;
             shadow_range = 4;
             shadow_render_power = 3;
-            "col.shadow" = "rgba(1a1a1aee)";
           };
 
         };
