a46606817861eeced26e57421f2e3651b7f9897f
bashfulrobot <dustin@bashfulrobot.com>
2024-10-09 09:54:38 -0700

    wip: waybar test
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   modules/desktops/hyprland/default.nix
    	new file:   modules/desktops/hyprland/waybar/config/audio.nix
    	new file:   modules/desktops/hyprland/waybar/config/backlight.nix
    	new file:   modules/desktops/hyprland/waybar/config/general.nix
    	new file:   modules/desktops/hyprland/waybar/config/hyprland.nix
    	new file:   modules/desktops/hyprland/waybar/config/network.nix
    	new file:   modules/desktops/hyprland/waybar/config/notification.nix
    	new file:   modules/desktops/hyprland/waybar/config/power.nix
    	new file:   modules/desktops/hyprland/waybar/config/recorder.nix
    	new file:   modules/desktops/hyprland/waybar/config/stats.nix
    	new file:   modules/desktops/hyprland/waybar/config/style.nix
    	modified:   modules/desktops/hyprland/waybar/default.nix

--- modules/desktops/hyprland/waybar/default.nix
+++ modules/desktops/hyprland/waybar/default.nix
@@ -20,148 +20,40 @@ in {
 
     home-manager.users."${user-settings.user.username}" = {
 
-      home.packages = [ pkgs.inter ];
+      imports = [
+        ./config/hyprland.nix
+        ./config/stats.nix
+        ./config/recorder.nix
+        ./config/backlight.nix
+        ./config/audio.nix
+        ./config/notification.nix
+        ./config/network.nix
+        ./config/general.nix
+        ./config/power.nix
 
-      services.playerctld.enable = true;
+        ./config/style.nix
+      ];
+
+      stylix.targets.waybar.enable = false;
+      wayland.windowManager.hyprland.settings = {
+        bind = [ "SUPER,b,exec,killall -SIGUSR1 .waybar-wrapped" ];
+        exec = [ "systemctl --user restart waybar" ];
+      };
 
       programs.waybar = {
         enable = true;
-        package = pkgs.waybar.overrideAttrs (oldAttrs: {
-          mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
-        });
+        systemd.enable = true;
         settings = {
           mainBar = {
-            margin = "0";
             layer = "top";
-            modules-left = [ "custom/nix" "wlr/workspaces" "mpris" ];
-            modules-center = [ "wlr/taskbar" ];
-            modules-right = [
-              "pulseaudio"
-              "network#interface"
-              "network#speed"
-              "cpu"
-              "temperature"
-              "clock"
-              "tray"
-            ];
-
-            persistent_workspaces = {
-              "1" = [ ];
-              "2" = [ ];
-              "3" = [ ];
-            };
-
-            "wlr/workspaces" = {
-              format = "{icon}";
-              on-click = "activate";
-              sort-by-number = true;
-              format-icons = {
-                "1" = '''';
-                "2" = ''󰈹'';
-                "3" = ''󰒱'';
-                "4" = ''󰧑'';
-              };
-            };
-
-            mpris = {
-              format =
-                "{status_icon}<span weight='bold'>{artist}</span> | {title}";
-              status-icons = {
-                playing = "󰎈";
-                paused = "󰏤";
-                stopped = "󰓛";
-              };
-            };
-
-            "custom/nix" = { format = "󱄅 "; };
-
-            "wlr/taskbar" = { on-click = "activate"; };
-
-            pulseaudio = {
-              format = "󰓃 {volume}%";
-            };
-
-            "network#interface" = {
-              format-ethernet = "󰣶 {ifname}";
-              format-wifi = "󰖩 {ifname}";
-              tooltip = true;
-              tooltip-format = "{ipaddr}";
-            };
-
-            "network#speed" = {
-              format =
-                "⇡ {bandwidthUpBits} ⇣ {bandwidthDownBits}";
-            };
-
-            cpu = {
-              format =
-                " {usage}% 󱐌 {avg_frequency}";
-            };
-
-            temperature = {
-              format =
-                "{icon} {temperatureC} °C";
-              format-icons = [ "" "" "" "󰈸" ];
-            };
-
-            clock = {
-              format = "  {:%H:%M}";
-              format-alt = "󰃭 {:%Y-%m-%d}";
-            };
-
-            tray = {
-              icon-size = 16;
-              spacing = 8;
-            };
+            position = "right";
+            margin = "5 2 5 0";
+            reload_style_on_change = true;
           };
         };
-
-        style = ''
-          * {
-            min-height: 0;
-          }
-
-          window#waybar {
-            font-family: 'Inter', 'RobotoMono Nerd Font';
-            font-size: 14px;
-          }
-
-          #custom-nix {
-            padding: 2px 8px;
-          }
-
-          #workspaces button {
-            padding: 2px 8px;
-            margin: 0 8px 0 0;
-          }
-
-          #workspaces button.active {
-          }
-
-          #taskbar button.active {
-          }
-
-          .modules-right * {
-            padding: 0 8px;
-            margin: 0 0 0 4px;
-          }
-
-          #mpris {
-            padding: 0 8px;
-            color: #8A889D;
-          }
-
-          #tray {
-            padding: 0 8px 0 8px;
-          }
-
-          #tray * {
-            padding: 0;
-            margin: 0;
-          }
-        '';
       };
 
     };
+
   };
 }
