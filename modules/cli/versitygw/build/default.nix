a1b98f5383d0154f57c9485e974f3aafd897ea95
bashfulrobot <dustin@bashfulrobot.com>
2024-11-11 11:43:35 -0800

    bug: used fetchurl when fetchzip was needed
    - changed funciton
    - moved strip root into the right spot

--- modules/cli/versitygw/build/default.nix
+++ modules/cli/versitygw/build/default.nix
@@ -3,11 +3,12 @@ let version = "1.0.8";
 in pkgs.stdenv.mkDerivation {
   name = "versitygw";
   dontConfigure = true;
-  stripRoot = false;
-  src = pkgs.fetchurl {
+
+  src = pkgs.fetchzip {
     url =
       "https://github.com/versity/versitygw/releases/download/v${version}/versitygw_v${version}_Linux_x86_64.tar.gz";
     sha256 = "sha256-eQOTh0Ybz1drtj1WLS9RXBI+O3fxbYGWUSI3TMGsTjE=";
+    stripRoot = false;
   };
   phases = [ "installPhase" ];
   installPhase = ''
