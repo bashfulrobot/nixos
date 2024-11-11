23f751cc2513e31c8ad7505e2dd376e5da7ac2f1
bashfulrobot <dustin@bashfulrobot.com>
2024-11-11 13:33:57 -0800

    bug: incorrect install phase path
    [modules/cli/versitygw/build/default.nix]
    - Change `stripRoot` to `true` in the `default.nix` file
    - fix path to the binary in the install phase

--- modules/cli/versitygw/build/default.nix
+++ modules/cli/versitygw/build/default.nix
@@ -8,12 +8,13 @@ in pkgs.stdenv.mkDerivation {
     url =
       "https://github.com/versity/versitygw/releases/download/v${version}/versitygw_v${version}_Linux_x86_64.tar.gz";
     sha256 = "sha256-kwIp8KIvdp2VK/2SEDp2aRc/eAbkHOZxFjiuxKFt77c=";
-    stripRoot = false;
+    stripRoot = true;
   };
   phases = [ "installPhase" ];
   installPhase = ''
     mkdir -p $out/bin
-    cp $src/versitygw $out/bin/versitygw
+    # ls $src/versitygw_v1.0.8_Linux_x86_64 > $out/bin/ls.txt
+    cp $src/versitygw_v1.0.8_Linux_x86_64/versitygw $out/bin/versitygw
     chmod +x $out/bin/versitygw
   '';
 
