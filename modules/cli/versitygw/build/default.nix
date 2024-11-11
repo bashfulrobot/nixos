930adc7ab2c9a1e8073aff853236f7c3c59e0943
bashfulrobot <dustin@bashfulrobot.com>
2024-11-11 11:36:23 -0800

    bug: incorrect sha256 for Verity Pkg
    - Updated to: sha256-eQOTh0Ybz1drtj1WLS9RXBI+O3fxbYGWUSI3TMGsTjE=

--- modules/cli/versitygw/build/default.nix
+++ modules/cli/versitygw/build/default.nix
@@ -7,7 +7,7 @@ in pkgs.stdenv.mkDerivation {
   src = pkgs.fetchurl {
     url =
       "https://github.com/versity/versitygw/releases/download/v${version}/versitygw_v${version}_Linux_x86_64.tar.gz";
-    sha256 = "sha256-ls5iOdSTFFj3j1pc9Qd2PYqS9YP/BPQBvvE6tVdR0HU=";
+    sha256 = "sha256-eQOTh0Ybz1drtj1WLS9RXBI+O3fxbYGWUSI3TMGsTjE=";
   };
   phases = [ "installPhase" ];
   installPhase = ''
