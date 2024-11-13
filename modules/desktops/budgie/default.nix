4ce361a046672d296095bac73e36cbafd68a74c2
bashfulrobot <dustin@bashfulrobot.com>
2024-11-12 16:44:49 -0800

    feat: Configure Budgie Desktop and related services
    - Enabling the Budgie Desktop with necessary services and configurations
    - Configuring the keymap in X11 and enabling the Budgie Desktop Manager in X11
    - Adding system packages for the `catppuccin-gtk` theme and related packages
    - Enabling various settings for the Budgie and GNOME desktop components
    
    [modules/desktops/budgie/default.nix]
    - Enable Budgie Desktop
    - Enable `dbus` service
    - Enable `gnome-keyring` service
    - Configure keymap in X11
    - Enable Budgie Desktop Manager in X11
    - Enable `lightdm` display manager
    - Configure `slick` greeter for `lightdm`
    - Enable `xdg.portal` with extra portals
    - Add system packages for `catppuccin-gtk` theme and related packages
    - Enable Kwallet for `lightdm`
    - Enable `dconf` settings for Budgie WM, Budgie Panel, Budgie Desktop View, GNOME Mutter, GNOME Desktop WM Preferences, GNOME Desktop Interface, GNOME Desktop Background, and GNOME Desktop Screensaver

--- modules/desktops/budgie/default.nix
+++ modules/desktops/budgie/default.nix
@@ -60,10 +60,39 @@ in {
     };
 
     environment = {
-      systemPackages = with pkgs;
-        [
-
-        ];
+      systemPackages = with pkgs; [
+        # valid options can be seen here - https://github.com/NixOS/nixpkgs/blob/7ce8e7c4cf90492a631e96bcfe70724104914381/pkgs/data/themes/catppuccin-gtk/default.nix#L16
+        (catppuccin-gtk.override {
+          accents = [
+            "blue"
+            "flamingo"
+            "green"
+            "lavender"
+            "maroon"
+            "mauve"
+            "peach"
+            "pink"
+            "red"
+            "rosewater"
+            "sapphire"
+            "sky"
+            "teal"
+            "yellow"
+          ]; # You can specify multiple accents here to output multiple themes
+          size = "compact"; # "standard" "compact"
+          tweaks = [
+            "black"
+            "rimless"
+            "normal"
+          ]; # You can also specify multiple tweaks here
+          variant = "mocha"; # "latte" "frappe" "macchiato" "mocha"
+        })
+        catppuccin-cursors
+        catppuccin-papirus-folders
+        catppuccinifier-gui
+        catppuccinifier-cli
+        work-sans
+      ];
       budgie.excludePackages = with pkgs;
         [
 
@@ -73,10 +102,14 @@ in {
     security.pam.services.lightdm.enableKwallet = true;
 
     programs.dconf.enable = true;
-    desktops.addons.wayland.enable = true;
     sys.catppuccin-theme.enable = true;
 
     home-manager.users."${user-settings.user.username}" = {
+
+      home.file."3pio-bender-catppuccin-mocha.png" = {
+        source = ../../sys/wallpapers/3pio-bender-catppuccin-mocha.png;
+        target = ".local/share/backgrounds/3pio-bender-catppuccin-mocha.png";
+      };
 
       home.sessionVariables.XDG_CURRENT_DESKTOP = "Budgie:GNOME";
 
@@ -87,15 +120,63 @@ in {
         components = [ "pkcs11" "secrets" "ssh" ];
       };
 
-      dconf.settings = with inputs.home-manager.lib.hm.gvariant;
-        {
-
+      dconf.settings = with inputs.home-manager.lib.hm.gvariant; {
+        "com/solus-project/budgie-wm" = {
+          enable-unredirect = true;
+          show-all-windows-tabswitcher = true;
+          edge-tiling = true;
         };
 
-      # home.file.".config/plasma-org.kde.plasma.desktop-appletsrc" = {
-      #   source = ./build/plasma-org.kde.plasma.desktop-appletsrc;
-      #   target = ".config/plasma-org.kde.plasma.desktop-appletsrc";
-      # };
+        "com/solus-project/budgie-panel" = {
+          notification-position = "BUDGIE_NOTIFICATION_POSITION_BOTTOM_RIGHT";
+          dark-theme = true;
+          builtin-theme = true;
+          enable-animations = true;
+        };
+
+        "org/buddiesofbudgie/budgie-desktop-view" = {
+          show = false;
+          click-policy = "double";
+        };
+
+        "org/gnome/mutter" = { edge-tiling = true; };
+
+        "org/gnome/desktop/wm/preferences" = { num-workspaces = 4; };
+
+        "org/gnome/desktop/interface" = {
+          font-name = "Work Sans 12";
+          document-font-name = "Work Sans 12";
+          monospace-font-name = "Source Code Pro 10";
+          font-hinting = "full";
+          font-antialiasing = "rgba";
+          gtk-theme = "catppuccin-mocha-sky-compact+black,rimless,normal";
+          icon-theme = "Tela-dark";
+          cursor-theme = "Bibata-Modern-Ice";
+          color-scheme = "prefer-dark";
+        };
+
+        "org/gnome/desktop/background" = {
+          picture-uri =
+            "file:///home/dustin/.local/share/backgrounds/3pio-bender-catppuccin-mocha.png";
+          picture-uri-dark =
+            "file:///home/dustin/.local/share/backgrounds/3pio-bender-catppuccin-mocha.png";
+          color-shading-type = "solid";
+          picture-options = "zoom";
+          primary-color = "#000000";
+          secondary-color = "#000000";
+        };
+
+        "org/gnome/desktop/screensaver" = {
+          picture-uri =
+            "file:///home/dustin/.local/share/backgrounds/3pio-bender-catppuccin-mocha.png";
+          # color-shading-type = "solid";
+          picture-options = "zoom";
+          # primary-color = "#000000";
+          # secondary-color = "#000000";
+        };
+
+      };
+
     };
 
   };
